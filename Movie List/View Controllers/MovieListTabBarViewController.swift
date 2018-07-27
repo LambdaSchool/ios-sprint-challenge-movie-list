c0e224a321a352c592300dea4138985de4145e73
Samantha Gatt <smfgatt@gmail.com>
2018-07-27 12:44:56 -0400

    Gives childrenVCs of TabBarVC the movieController value

--- Movie List/View Controllers/MovieListTabBarViewController.swift
+++ Movie List/View Controllers/MovieListTabBarViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class MovieListTabBarViewController: UITabBarController, MovieControllerProtocol {
+class MovieListTabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,8 +21,22 @@ class MovieListTabBarViewController: UITabBarController, MovieControllerProtocol
     // Function to assign its children's movieController var the value of its own movieController
     func passMovieControllerToChildVCs() {
         
+        // Creates an array of children VCs stored in the constant childrenVCs
+        guard let childrenVCs = self.viewControllers else { return }
+        
+        for childVC in childrenVCs {
+            
+            // Told me conformingVC.movieController couldn't be set since it was a let constant so...
+            if var conformingVC = childVC as? MovieControllerProtocol {
+                
+                // Assigns its children's movieController attribute to the value of its own
+                conformingVC.movieController = movieController
+            }
+        }
+        
+        
     }
 
-    
-    var movieController: MovieController?
+    // Declares an empty instance of MovieController so it can control instances of Movie and make a movies array
+    let movieController = MovieController()
 }
