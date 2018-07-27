11cea48ecb4afe3c3a8959a3e3957c6bddf116e3
Conner Alegre <cga0@pm.me>
2018-07-27 09:38:36 -0600

    Setup Storyboard files and Movie Model

---
+++ Movie List/Models/Movie.swift
@@ -0,0 +1,14 @@
+//
+//  Movie.swift
+//  Movie List
+//
+//  Created by Conner on 7/27/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Movie {
+    var movieName: String
+    var isSeen: String
+}
