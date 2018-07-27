395f18c8bfca96cfa6df2f6ae2fb11f7cdf4af65
Jeremy Taylor <jtaylor@jctaylor.org>
2018-07-27 10:37:32 -0500

    Initial Storyboard Setup done

---
+++ Movie List/Views/MovieTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieTableViewCell.swift
+//  Movie List
+//
+//  Created by Jeremy Taylor on 7/27/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    
+
+}
