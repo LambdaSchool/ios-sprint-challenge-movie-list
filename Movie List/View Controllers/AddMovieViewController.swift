00475595eb460d59e206811c28eb41633cab472b
Conner Alegre <cga0@pm.me>
2018-07-27 10:12:26 -0600

    add view controllers

---
+++ Movie List/View Controllers/AddMovieViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AddMovieViewController.swift
+//  Movie List
+//
+//  Created by Conner on 7/27/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class AddMovieViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
