7dfd217b79ef2fc27b2a9c7a4cf7300b1d33a299
Carolyn Lea <carolynrlea@gmail.com>
2018-07-27 10:12:20 -0600

    Initial Commit

--- Movie List/AppDelegate.swift
+++ Movie List/AppDelegate.swift
@@ -15,7 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        UINavigationBar.appearance().largeTitleTextAttributes = [NSAttributedStringKey.foregroundColor: UIColor.black, NSAttributedStringKey.font: UIFont(name: "Avenir-Black", size: 30) ?? UIFont.systemFont(ofSize: 30)]
+        
         return true
     }
 
