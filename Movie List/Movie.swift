e63918e61dee91662958c42b78bb4b2cba8c12d1
jWarrenDev <Jerrick.Warren@gmail.com>
2018-08-31 08:33:52 -0700

    initial commit

---
+++ Movie List/Movie.swift
@@ -0,0 +1,17 @@
+//
+//  Movie.swift
+//  Movie List
+//
+//  Created by Welinkton on 8/31/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Movie {
+    let name: String
+    
+    init(name: String){
+        self.name = name
+    }
+}
