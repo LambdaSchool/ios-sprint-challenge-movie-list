ffce388b59157882804fd9fa0ebb89fb415fbc93
Ilqar Ilyasov <imilqar@gmail.com>
2018-08-31 10:54:22 -0500

    Created 2 Stroyboard. AddMovie.storyboard for adding new movie to the list. MovieList.storyboard for showing all the submitted movies. Also added 2 Cocoa Touch Subclass to have access the outlets and actions.

---
+++ Controllers/AddMovieViewController.swift
@@ -0,0 +1,24 @@
+//
+//  AddMovieViewController.swift
+//  Movie List
+//
+//  Created by Ilgar Ilyasov on 8/31/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class AddMovieViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func addMovie(_ sender: Any) {
+        
+    }
+    
+    @IBOutlet weak var movieTitle: UITextField!
+}
