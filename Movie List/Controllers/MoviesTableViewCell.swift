23bcd4fc69cc0c7b1c6ff2b22de896a4061306d7
Scott Bennett <stt.btt@gmail.com>
2018-08-31 13:57:21 -0400

    Commit for sprint deadline

---
+++ Movie List/Controllers/MoviesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MoviesTableViewCell.swift
+//  Movie List
+//
+//  Created by Scott Bennett on 8/31/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MoviesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
