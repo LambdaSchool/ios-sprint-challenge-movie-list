18409480aa016bea6fe0ecb6b139dc0d9f2def13
Ilqar Ilyasov <imilqar@gmail.com>
2018-08-31 11:32:39 -0500

    Added TableViewCell and related outlets.

---
+++ Movie List/MovieListTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieListTableViewCell.swift
+//  Movie List
+//
+//  Created by Ilgar Ilyasov on 8/31/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MovieListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    @IBAction func movieListSeenUnseenAction(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var movieListLabel: UILabel!
+}
