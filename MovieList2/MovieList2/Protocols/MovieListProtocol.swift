d3d51631c2794eeeda7f6f2848bcc0f98cde4c02
jWarrenDev <Jerrick.Warren@gmail.com>
2018-09-07 08:37:57 -0700

    added protocol

---
+++ MovieList2/MovieList2/Protocols/MovieListProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  MovieListProtocol.swift
+//  MovieList2
+//
+//  Created by Welinkton on 9/7/18.
+//  Copyright © 2018 Jerrick Warren. All rights reserved.
+//
+
+import Foundation
+
+protocol MovieListProtocol:class {
+    var movieController: MovieController? {get set}
+}
