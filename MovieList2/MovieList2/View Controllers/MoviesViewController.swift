05c6dfd08a439a0b5437eda7302dc7127f925656
jWarrenDev <Jerrick.Warren@gmail.com>
2018-09-12 13:59:08 -0700

    TV -> VC

--- MovieList2/MovieList2/View Controllers/MoviesViewController.swift
+++ MovieList2/MovieList2/View Controllers/MoviesViewController.swift
@@ -8,9 +8,11 @@
 
 import UIKit
 
-class MoviesTableViewController: UITableViewController, MovieListProtocol, MovieTableCellDelegate {
+class MoviesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, MovieListProtocol, MovieTableCellDelegate {
     
     var movieController: MovieController?
+    
+    @IBOutlet var tableView: UITableView!
     
     // method to update custom movie cell
     func updateMovieCell(for cell: MoviesTableViewCell) {
@@ -31,12 +33,12 @@ class MoviesTableViewController: UITableViewController, MovieListProtocol, Movie
     // MARK: - Table view data source
     
     // references array to see how many rows
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return movieController?.movies.count ?? 0
     }
 
     // grabs the cell.. so that you can customize it
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
        guard let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as? MoviesTableViewCell,
         let movie = movieController?.movies[indexPath.row] else {return UITableViewCell()}
         
@@ -50,7 +52,7 @@ class MoviesTableViewController: UITableViewController, MovieListProtocol, Movie
     }
     
     // Function to delete Cells
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             guard let movie = movieController?.movies[indexPath.row] else {return}
             
