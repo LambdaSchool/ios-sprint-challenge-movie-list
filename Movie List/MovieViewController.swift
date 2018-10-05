c4f00dc261d59c1c694f3359e48a0276ca161280
jWarrenDev <Jerrick.Warren@gmail.com>
2018-10-05 08:35:10 -0700

    storyboard completed

---
+++ Movie List/MovieViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MovieViewController.swift
+//  Movie List
+//
+//  Created by Welinkton on 10/5/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MovieViewController: UIViewController {
+    
+}
---
+++ Movie List/ResultsMovieViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ResultsMovieViewController.swift
+//  Movie List
+//
+//  Created by Welinkton on 10/5/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ResultsMovieViewController: UIViewController {
+    
+}
