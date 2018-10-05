6ec3e5460afc247aa079c207434d0296be01010d
Lotanna <sugabelly@gmail.com>
2018-10-05 11:14:51 -0400

    Exam Begins

---
+++ Movie List/Movie List/AddMovieViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AddMovieViewController.swift
+//  Movie List
+//
+//  Created by Lotanna Igwe-Odunze on 10/5/18.
+//  Copyright © 2018 Lotanna. All rights reserved.
+//
+
+import UIKit
+
+class AddMovieViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Movie List/Movie List/MoviesViewController.swift
@@ -0,0 +1,38 @@
+//
+//  MoviesViewController.swift
+//  Movie List
+//
+//  Created by Lotanna Igwe-Odunze on 10/5/18.
+//  Copyright © 2018 Lotanna. All rights reserved.
+//
+
+import UIKit
+
+class MoviesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+
+    
+    
+    @IBOutlet weak var movieTable: UITableView!
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
