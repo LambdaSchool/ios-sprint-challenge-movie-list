e9b917bcfea59b70bbcc7c91458842873b56853d
Moses Robinson <mosesjrobinson@icloud.com>
2018-10-05 11:11:53 -0700

    trying to make buttons work

--- Movie List/MovieModel.swift
+++ Movie List/MovieModel.swift
@@ -3,17 +3,34 @@ import UIKit
 
 class MovieModel: NSObject, UITableViewDataSource, MovieTableViewCellDelegate {
     
+    var movies: [Movie] = []
+    
     
     func tappedSeenButton(on cell: MovieTableViewCell) {
-        <#code#>
+//        guard let indexPath = tableView?.indexPath(for: cell)
+//            else { fatalError("non-existent cell!!??")}
+//
+//        movies[indexPath.row].isSeen.toggle()
+//        cell.isSeen = movies[indexPath.row].isSeen ? "Seen" : "Not Seen"
     }
+    
+    
+    weak var tableView: UITableView?
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return movies.count
     }
     
+    
+    
+    let reuseIdentifierCell = "cell"
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifierCell) as? MovieTableViewCell else {
+            fatalError("Could not instantiate guranteed cell type")
+        }
+        // code for cells interation.
+        return cell
     }
     
     
