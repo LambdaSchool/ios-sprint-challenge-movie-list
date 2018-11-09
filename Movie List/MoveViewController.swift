971b59dce084acdbcbf30c4b0188deb7ee1db763
arielmclaughlinn <arielmclaughlin@icloud.com>
2018-11-09 09:41:54 -0700

    Model complete. Allows user to add, remove, etc. Saves the data.

--- Movie List/MoveViewController.swift
+++ Movie List/MoveViewController.swift
@@ -1,9 +1,4 @@
-//
-//  MoveViewController.swift
-//  Movie List
-//
-//  Created by Ariel M. McLaughlin on 11/9/18.
-//  Copyright © 2018 Lambda School. All rights reserved.
-//
-
 import Foundation
+import UIKit
+
+
