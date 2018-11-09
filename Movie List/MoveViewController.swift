b4754ff2104a189e055570ab6b85378c1948fffd
arielmclaughlinn <arielmclaughlin@icloud.com>
2018-11-09 09:19:51 -0700

    Storyboard setup complete.

---
+++ Movie List/MoveViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MoveViewController.swift
+//  Movie List
+//
+//  Created by Ariel M. McLaughlin on 11/9/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import Foundation
