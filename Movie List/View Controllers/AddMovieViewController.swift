ea30b3ae22971a97cd5b6f2df3a175e51b2b4e8f
Jonah Bergevin <jonahbergevin@gmail.com>
2018-11-09 09:48:34 -0700

    Build View Controllers as well as table view delegate and datasource

--- Movie List/View Controllers/AddMovieViewController.swift
+++ Movie List/View Controllers/AddMovieViewController.swift
@@ -6,6 +6,9 @@ class AddMovieViewController: UIViewController {
     @IBOutlet weak var addMovieTextField: UITextField!
     
     @IBAction func AddMovieButton(_ sender: Any) {
+        guard let text = addMovieTextField.text, !text.isEmpty else { return }
+        
+        Model.shared.addMovie(text)
     }
     
 }
--- Movie List/View Controllers/MovieListViewController.swift
+++ Movie List/View Controllers/MovieListViewController.swift
@@ -1,10 +1,30 @@
 import UIKit
 
-class MovieListViewController: UIViewController {
+class MovieListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.reloadData()
+    }
     
     @IBOutlet weak var tableView: UITableView!
     
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Model.shared.movieCount()
+    }
+    
     let reuseIdentifier = "MovieCell"
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        
+        cell.textLabel?.text = Model.shared.movies[indexPath.row]
+        
+        return cell
+    }
     
 }
