118839ddc815a898be673cab9107d59558f7ba5d
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2018-11-09 11:28:40 -0500

    add a view controller file for another view controller

---
+++ Movie List/MovieViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MovieViewController.swift
+//  Movie List
+//
+//  Created by Julian A. Fordyce on 11/9/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import Foundation
