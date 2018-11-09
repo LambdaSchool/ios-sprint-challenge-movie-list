cb4d17fe70021d32f7813c93130c3ed0ea779a9b
Meera Andersen <meera.andersen@gmail.com>
2018-11-09 10:43:09 -0700

    added number of rows in section, and cell for row at

--- Sprint1_Challenge/Sprint1_Challenge/MovieList.swift
+++ Sprint1_Challenge/Sprint1_Challenge/MovieList.swift
@@ -1,2 +1,35 @@
+import UIKit
 
-import Foundation
+class MovieListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBAction func add(_ sender: Any) {
+        guard let text = textField.text, !text.isEmpty else { return }
+        Model.shared.addItem(text)
+        textField.text = nil
+        tableView.insertRows(at: [IndexPath(row: Model.shared.itemCount() - 1, section: 0)], with: .top)
+    }
+    let reuseIdentifier = "cell"
+    
+    override func viewWillAppear() {
+        super.viewWillAppear(animated)
+        
+        tableView.datasource = self
+        tableView.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Model.shared.itemCount()
+
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell  = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        cell.textLabel?.text = Model.shared.item(at: indexPath.row)
+    }
+}
+
+
