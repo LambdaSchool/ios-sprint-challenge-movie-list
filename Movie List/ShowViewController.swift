47eb41e0b99e17f18d62a1a9661e83a13f2c06c7
Sean Doyle <sdoylelambda@gmail.com>
2018-11-09 15:25:05 -0500

    app working properly

--- Movie List/ShowViewController.swift
+++ Movie List/ShowViewController.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-class ShowViewController: UIViewController  {// , UITableViewDelegate, UITableViewDataSource {
+class ShowViewController: UIViewController , UITableViewDelegate, UITableViewDataSource {
 
 //     navigationController?.popViewController(animated: true)
 //
@@ -8,41 +8,39 @@ class ShowViewController: UIViewController  {// , UITableViewDelegate, UITableVi
 //
 //    let tableView = Model.item
     
-    @IBOutlet weak var textView: UITableView!
+    @IBOutlet weak var tableView: UITableView!
 //
-//    override func viewWillAppear(_ animated: Bool) {
-//        super.viewWillAppear(animated)
+    override func viewWillAppear(_ animated: Bool) {
+                super.viewWillAppear(animated)
+                tableView.dataSource = self
+                tableView.delegate = self
+                tableView.reloadData()
+            }
 //
-//        AddViewController.tableView.dataSource = self
-//        tableView.delegate = self
-//        tableView.reloadData()
-//    }
-//
-//
-//    let reuseIdentifier = "cell"
-//
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return Model.shared.itemCount()
-//    }
-//
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
-//        cell.textLabel?.text = Model.shared.item(at: indexPath.row)
-//        return cell
-//    }
-//
-//    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-//        guard editingStyle == .delete else { return }
-//        Model.shared.removeValue(at: indexPath.row)
-//        tableView.deleteRows(at: [indexPath], with: .fade)
-//
-//    }
-//
-//    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
-//        Model.shared.moveItem(from: sourceIndexPath.row, to: destinationIndexPath.row)
-//        tableView.moveRow(at: sourceIndexPath, to: destinationIndexPath)
-//    }
-//
+    let reuseIdentifier = "cell"
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Model.shared.itemCount()
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        cell.textLabel?.text = Model.shared.item(at: indexPath.row)
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        guard editingStyle == .delete else { return }
+        Model.shared.removeValue(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .fade)
+
+    }
+
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        Model.shared.moveItem(from: sourceIndexPath.row, to: destinationIndexPath.row)
+        tableView.moveRow(at: sourceIndexPath, to: destinationIndexPath)
+    }
+
 }
 //
 //
