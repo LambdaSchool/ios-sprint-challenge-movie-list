4c43952c9aba0c5eeaffe45f0b4c1c8c20762235
Jaspal Suri <44656000+JaspalSuri@users.noreply.github.com>
2018-11-09 10:59:06 -0800

    cleand out some comments

--- Movie List/ViewController.swift
+++ Movie List/ViewController.swift
@@ -8,13 +8,9 @@ class ViewController: UIViewController {
         guard let text = textField.text, !text.isEmpty else { return }
         Model.shared.addMovie(text)
         textField.text = nil
-        // TableViewController.tableView.insertRows(at: [IndexPath(row: Model.shared.movieCount() - 1, section: 0)], with: .top)
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        // tableView.dataSource = self
-        // tableView.delegate = self
-        // tableView.reloadData()
     }
     
    
