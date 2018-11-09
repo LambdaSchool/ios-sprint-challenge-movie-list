aa3b61117eec78f3bbc716135c8f22a5e2ac4ffa
Sean Doyle <sdoylelambda@gmail.com>
2018-11-09 12:43:32 -0500

    working app, incorrct layout

--- Movie List/AddViewController.swift
+++ Movie List/AddViewController.swift
@@ -2,45 +2,45 @@
 import UIKit
 
 class AddViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-    
-    
+
+
     @IBOutlet weak var tableView: UITableView!
-    
+
     @IBOutlet weak var textField: UITextField!
-    
+
     @IBAction func add(_ sender: Any) {
         guard let text = textField.text, !text.isEmpty else { return }
         Model.shared.addItem(text)
         textField.text = nil
         tableView.reloadData()
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         tableView.dataSource = self
         tableView.delegate = self
         tableView.reloadData()
     }
-    
+
     let reuseIdentifier = "cell"
-    
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return Model.shared.itemCount()
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
         cell.textLabel?.text = Model.shared.item(at: indexPath.row)
         return cell
     }
-    
+
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         guard editingStyle == .delete else { return }
         Model.shared.removeValue(at: indexPath.row)
         tableView.deleteRows(at: [indexPath], with: .fade)
-        
+
     }
-    
+
     func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
         Model.shared.moveItem(from: sourceIndexPath.row, to: destinationIndexPath.row)
         tableView.moveRow(at: sourceIndexPath, to: destinationIndexPath)
