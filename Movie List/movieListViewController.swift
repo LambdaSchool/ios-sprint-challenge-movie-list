02eb13ec7ae09a84855af33272423fbd6e26dbd0
IvanCaldwell <grumpy.caldwell@gmail.com>
2018-11-09 08:59:21 -0800

    Added Model.swift

--- Movie List/movieListViewController.swift
+++ Movie List/movieListViewController.swift
@@ -5,15 +5,23 @@
 //  Created by Ivan Caldwell on 11/9/18.
 //  Copyright © 2018 Lambda School. All rights reserved.
 //
-
+import Foundation
 import UIKit
 
-class movieListViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+class movieListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    let reuseIdentifier = "cell"
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Model.shared.movieCount()
     }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        cell.textLabel?.text = Model.shared.movie(at: indexPath.row)
+        return cell
+    }
+    
+    
 
 }
