52b644e457bd9be7e9a77db3eacb9c1e984f02c7
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2018-11-09 11:26:24 -0500

    add a new view controller file

---
+++ Movie List/AddMovieViewController.swift
@@ -0,0 +1,9 @@
+//
+//  AddMovieViewController.swift
+//  Movie List
+//
+//  Created by Julian A. Fordyce on 11/9/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import Foundation
