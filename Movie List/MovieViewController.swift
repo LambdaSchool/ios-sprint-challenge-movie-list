478ec45c5d7e8d5474a876e2c395f4e6ded52422
arielmclaughlinn <arielmclaughlin@icloud.com>
2018-11-09 10:14:38 -0700

    User can now re-order movies.

--- Movie List/MovieViewController.swift
+++ Movie List/MovieViewController.swift
@@ -5,6 +5,8 @@ class MovieViewController: UIViewController, UITableViewDelegate, UITableViewDat
     
     @IBOutlet weak var movieText: UITextField!
     
+    @IBOutlet var tableView: UITableView!
+    
     @IBAction func add(_ sender: Any) {
         
         guard let text = movieText.text, !text.isEmpty else { return }
@@ -17,6 +19,7 @@ class MovieViewController: UIViewController, UITableViewDelegate, UITableViewDat
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return Model.shared.movieCount()
     }
+
     
     let reuseIdentifier = "movieCell"
     
@@ -25,6 +28,32 @@ class MovieViewController: UIViewController, UITableViewDelegate, UITableViewDat
         cell.textLabel?.text = Model.shared.movie(at: indexPath.row)
         return cell
     }
+    
+    private func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowat indexPath: IndexPath) {
+        
+        guard editingStyle == .delete else { return }
+        
+        Model.shared.removeMovie(at: indexPath.row)
+        
+        tableView.deleteRows(at: [indexPath], with: .fade)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    @IBAction func edit(_ sender: Any) {
+        tableView.setEditing(true, animated: true)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(stopEditingTable(_:)))
+    }
+    
+    @objc
+    func stopEditingTable(_ sender: Any) {
+        tableView.setEditing(false, animated: true)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(edit(_:)))
+    }
+
+    
+    
     
     
 }
