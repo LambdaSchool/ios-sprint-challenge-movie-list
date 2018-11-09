45a864333b5cb652b1a126ba3273456234acdba1
Sergey Osipyan <osipyansergey@gmail.com>
2018-11-09 11:05:05 -0800

    Not finish yet, stop on error trying to fix

--- Movie List/AddMovieViewController.swift
+++ Movie List/AddMovieViewController.swift
@@ -1,1 +1,18 @@
+import UIKit
 
+class AddMovieViewController: UIViewController {
+    
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBAction func add(_ sender: Any) {
+        guard let text = textField.text,
+            !text.isEmpty else {return}
+        
+        Model.shared.addMovie(text)
+        textField.text = nil
+    }
+    
+}
--- Movie List/MovieListViewController.swift
+++ Movie List/MovieListViewController.swift
@@ -1,1 +1,54 @@
+import UIKit
 
+class MovieListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    //establishing delegate
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.reloadData()
+    }
+    let reuseIdentifier = "cell"
+    
+    //table functions
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Model.shared.movieCount()
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        
+        cell.textLabel?.text = Model.shared.movie(at: indexPath.row)
+        return cell
+    }
+    
+    //table functions for editing
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        Model.shared.moveMovie(at: sourceIndexPath.row, to: destinationIndexPath.row)
+        tableView.moveRow(at: sourceIndexPath, to: destinationIndexPath)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        guard editingStyle == .delete else {return}
+        Model.shared.removeMovie(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .fade)
+    }
+    
+    //objective c functions that use #selector makes it fancy
+    @IBAction func editTable(_ sender: Any) {
+        tableView.setEditing(true, animated: true)
+        navigationItem.rightBarButtonItem =
+            UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(stopEditingTable(_:)))
+    }
+    
+    @objc
+    func stopEditingTable(_ sender: Any) {
+        tableView.setEditing(false, animated: true)
+        navigationItem.rightBarButtonItem =
+            UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(editTable(_:)))
+    }
+}
