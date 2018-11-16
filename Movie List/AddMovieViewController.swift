3098eb27278150d2f97abd5572f6bc07afb67b26
Vijay Das <vijaytdas@gmail.com>
2018-11-16 10:33:12 -0600

    add files for the two view controllers

---
+++ Movie List/AddMovieViewController.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+class AddMovieViewController: UIViewController {
+    // This controller is for the add screen which will have:
+    // (screen will a label for a "Add a new movie to the list")
+    // a textField for user to enter movie -> iboutlet
+    // an add movie button - ibaction
+    // and addMovie call to model to add to array
+    // first guarding for bad entries from user
+    
+    // Outlet from textField here:
+    
+    
+    
+    
+    
+    // Action and function to add movie
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
---
+++ Movie List/MovieListViewController.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+class MovieListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // assign the tableView data source and delegate her
+        
+        
+        
+        
+    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+        
+}
