32b68800e04f133a22517e4f0da6c7f65918d18b
Stuart Sippel <stuartsippel@icloud.com>
2018-11-16 10:59:18 -0600

    set up storyboard, tab bar controller, and associated view controllers

---
+++ Movie List/MyMoviesViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MyMoviesViewController.swift
+//  Movie List
+//
+//  Created by Stuart on 11/16/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MyMoviesViewController: UIViewController {
+    
+}
---
+++ Movie List/TabBarController.swift
@@ -0,0 +1,13 @@
+//
+//  TabBarController.swift
+//  Movie List
+//
+//  Created by Stuart on 11/16/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+    
+}
