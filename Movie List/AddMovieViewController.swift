3e696fc44d000d8db0660c0f44f48677f38e1bbe
Audrey Welch <welch.audreyl@gmail.com>
2018-11-16 11:47:16 -0600

    Set up the Movies View Controller

--- Movie List/AddMovieViewController.swift
+++ Movie List/AddMovieViewController.swift
@@ -7,12 +7,15 @@ import UIKit
 
 class AddMovieViewController: UIViewController {
     
+    
+    
     @IBAction func addMovieButton(_ sender: Any) {
         
         // guard against an empty text field
         guard let newMovie = textField.text, !newMovie.isEmpty else { return }
         
-        
+        // update the model with the new movie
+        Model.shared.addMovie(newMovie)
         
     }
     
--- Movie List/MoviesViewController.swift
+++ Movie List/MoviesViewController.swift
@@ -5,6 +5,71 @@
 
 import UIKit
 
-class MoviesViewController: UIViewController {
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    let reuseIdentifier = "cell"
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // set the object that acts as the data source to the UIViewController class
+        tableView.dataSource = self
+        
+        // set the object that acts as the delegate to the UIViewController class
+        tableView.delegate = self
+        
+        // reload data
+        tableView.reloadData()
+    }
+    
+    // MARK: - Table View Data Source
+    // Data Source provides the number of rows that is equal to the number of movies in the Model
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Model.shared.movieCount()
+    }
+    
+    // Data Source provides what data the cells should contain
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // get a cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        // get a movie from the model
+        let movie = Model.shared.movie(at: indexPath.row)
+        
+        // set up the cell with the model object that we have
+        cell.textLabel?.text = movie
+        
+        // return the cell back to the table view
+        return cell
+    }
+    
+    // Data Source commits insertion or deletion at a specified row
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        // delete items
+        guard editingStyle == .delete else { return }
+        
+        // update Model by removing movie from the Model
+        Model.shared.removeMovie(at: indexPath.row)
+        
+        // update table view by removing it from the table view with a swipe
+        tableView.deleteRows(at: [indexPath], with: .automatic)
+    }
+    
+    // Data Source commits row re-ordering
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        
+        // call the Model's move action and update the Model
+        Model.shared.moveMovie(from: sourceIndexPath.row, to: destinationIndexPath.row)
+        
+        // update the table view
+        tableView.moveRow(at: sourceIndexPath, to: destinationIndexPath)
+    }
     
 }
