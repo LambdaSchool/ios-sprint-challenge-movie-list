e45271e86b149df0561d6757802b62c0dcca51cb
Meera Andersen <meera.andersen@gmail.com>
2018-11-16 09:25:09 -0700

    add methods for adding, removing, and moving to Model

--- MBP Sprint Challenge/MBP Sprint Challenge/Model.swift
+++ MBP Sprint Challenge/MBP Sprint Challenge/Model.swift
@@ -8,13 +8,24 @@ class Model {
     private var movies: [String] = []
     
     
-    //func movieCount
+    func movieCount() -> Int {
+        return movies.count
+    }
     
-    //
+    func movie(at index: Int) -> String {
+        return movies[index]
     
-//func addMovie
+    func addMovie(_ movie: String) {
+        movies.append(movie)
+    }
     
-    //func removeMovie
     
-    //func moveMovie
+    func removeMovie(at index: Int) {
+        movies.remove(at: index)
+    }
+    
+        func moveMovie(from index: Int, to newIndex: Int) {
+            let movie = movies.remove(at: index)
+            movies.insert(movie, at: newIndex)
+        }
 }
