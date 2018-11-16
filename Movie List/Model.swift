ff5837999dc043deffb398ed75a0963db6ff3702
Sean Doyle <sdoylelambda@gmail.com>
2018-11-16 11:32:21 -0500

    singleton, array and add item added to model

--- Movie List/Model.swift
+++ Movie List/Model.swift
@@ -1,5 +1,11 @@
 import Foundation
 
 class Model {
+    static let shared = Model()
+    private init() {}
+    private var items: [String] = []
     
+    func addItem(_ item: String) {
+        items.append(item)
+    }
 }
