c41ac0a7d2df0b5891a092bf873fa15a14744d46
Meera Andersen <meera.andersen@gmail.com>
2018-11-16 09:30:52 -0700

    built MVC

--- MBP Sprint Challenge/MBP Sprint Challenge/AddMovieViewController.swift
+++ MBP Sprint Challenge/MBP Sprint Challenge/AddMovieViewController.swift
@@ -7,6 +7,9 @@ class AddMovieViewController: UIViewController {
     
     
     @IBAction func addMovie(_ sender: Any) {
+        guard let text = textField.text, !text.isEmpty else { return }
+        Model.shared.addMovie(text)
+        
     }
     
     override func viewDidLoad() {
--- MBP Sprint Challenge/MBP Sprint Challenge/Model.swift
+++ MBP Sprint Challenge/MBP Sprint Challenge/Model.swift
@@ -14,6 +14,7 @@ class Model {
     
     func movie(at index: Int) -> String {
         return movies[index]
+    }
     
     func addMovie(_ movie: String) {
         movies.append(movie)
@@ -24,8 +25,23 @@ class Model {
         movies.remove(at: index)
     }
     
-        func moveMovie(from index: Int, to newIndex: Int) {
-            let movie = movies.remove(at: index)
-            movies.insert(movie, at: newIndex)
+    func moveMovie(from index: Int, to newIndex: Int) {
+        let movie = movies.remove(at: index)
+        movies.insert(movie, at: newIndex)
+    }
+    
+    let fileURL = URL(fileURLWithPath: NSHomeDirectory())
+        .appendingPathComponent("Library")
+        .appendingPathComponent("Movies")
+        .appendingPathComponent("plist")
+    
+    func saveData() {
+        try! (movies as NSArray).write(to: fileURL)
+    }
+    
+    func loadData() {
+        if let items = NSArray(contentsOf: fileURL) as? [String] {
+            movies = items
         }
+    }
 }
