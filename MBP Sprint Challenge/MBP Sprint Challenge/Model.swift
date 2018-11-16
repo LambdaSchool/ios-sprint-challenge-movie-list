ea5bb34e02d44b3a97705eae5c28524c446c5539
Meera Andersen <meera.andersen@gmail.com>
2018-11-16 09:10:55 -0700

    built UI for add movie view controller and embedded in tab bar controller

---
+++ MBP Sprint Challenge/MBP Sprint Challenge/Model.swift
@@ -0,0 +1,9 @@
+//
+//  Model.swift
+//  MBP Sprint Challenge
+//
+//  Created by Meera Andersen on 11/16/18.
+//  Copyright © 2018 Meera Andersen. All rights reserved.
+//
+
+import Foundation
---
+++ MBP Sprint Challenge/MBP Sprint Challenge/MovieListViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class MovieListViewController: UIViewController {
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    
+}
+
