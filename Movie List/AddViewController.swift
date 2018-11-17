c96b5c8f8d95fbbb1be416ae32145776dbb910cd
Sean Doyle <sdoylelambda@gmail.com>
2018-11-17 09:11:14 -0500

    Label displays movie added with string interpolation.

--- Movie List/AddViewController.swift
+++ Movie List/AddViewController.swift
@@ -4,9 +4,12 @@ class AddViewController: UIViewController {
     
     @IBOutlet weak var textField: UITextField!
     
+    @IBOutlet weak var labelText: UILabel!
+    
     @IBAction func addButton(_ sender: Any) {
         guard let text = textField.text, !text.isEmpty else { return }
         Model.shared.addItem(text)
+        labelText.text = "You added \(text)"
         textField.text = nil
     }
 }
