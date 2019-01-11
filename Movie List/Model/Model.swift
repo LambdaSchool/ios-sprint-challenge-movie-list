304fe75b5756e809d80b7e96c47e6db2e2ef6765
Sean Doyle <sdoylelambda@gmail.com>
2019-01-11 13:32:31 -0500

    User data saves between subsequent runs of application.

--- Movie List/Model/Model.swift
+++ Movie List/Model/Model.swift
@@ -8,15 +8,33 @@ class Model {
     
     func addMovie(_ movie: String) {
         movies.append(movie)
+        saveData()
     }
     
     func deleteMovie(at index: Int) {
         movies.remove(at: index)
+        saveData()
     }
     
     func moveMovie(from index: Int, to destination: Int) {
         let value = movies.remove(at: index)
         movies.insert(value, at: destination)
+        saveData()
     }
+    
+    func saveData() {
+        try! (movies as NSArray).write(to: fileURL)
+    }
+    
+    func loadData() {
+        if let items = NSArray(contentsOf: fileURL) as? [String] {
+        movies = items
+        }
+    }
+    
+    let fileURL = URL(fileURLWithPath: NSHomeDirectory())
+        .appendingPathComponent("Library")
+        .appendingPathComponent("Movies")
+        .appendingPathExtension("plist")
 }
 
--- Movie List/View Controllers/MovieTableViewCell.swift
+++ Movie List/View Controllers/MovieTableViewCell.swift
@@ -3,15 +3,15 @@ import UIKit
 class MovieTableViewCell: DisplayMoviesTableViewController {
 
     @IBAction func seenOrNotAction(_ sender: Any) {
-        tableView.setEditing(true, animated: true)
-        DisplayMoviesTableViewController.cell.seenOrNotAction = UIButtonItem
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(stopEditingTable(_:)))
-    }
-    
-    @objc
-    override func stopEditingTable(_ sender: Any) {
-        tableView.setEditing(false, animated: true)
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(editAction(_:)))
+//        tableView.setEditing(true, animated: true)
+//        DisplayMoviesTableViewController.cell.seenOrNotAction = UIButtonItem
+//        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(stopEditingTable(_:)))
+//    }
+//
+//    @objc
+//    override func stopEditingTable(_ sender: Any) {
+//        tableView.setEditing(false, animated: true)
+//        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(editAction(_:)))
     }
     
     override func awakeFromNib() {
