55aa18278207c01031b48ee66e0364c906d27f3d
Dustin Koch <dustin.koch@me.com>
2019-01-11 12:07:10 -0600

    App completion but debugging errors

---
+++ Movie List/View Controllers/AddMovieViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AddMovieViewController.swift
+//  Movie List
+//
+//  Created by Dustin Koch on 1/11/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class AddMovieViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    
+    
+
+
+}
