7527a21e666bbc323144a608417e4372d5e05a69
Nelson Gonzalez <nelglezfl@gmail.com>
2019-01-11 11:35:03 -0500

    Add tableView data source and delegate methods and tableview cell with label/outlet and button/action

---
+++ Movie List/View Controllers/AddMovieViewController.swift
@@ -0,0 +1,27 @@
+//
+//  AddMovieViewController.swift
+//  Movie List
+//
+//  Created by Nelson Gonzalez on 1/11/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class AddMovieViewController: UIViewController {
+    
+    @IBOutlet weak var textView: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func addButton(_ sender: UIButton) {
+    }
+    
+
+}
---
+++ Movie List/View Controllers/MovieListViewController.swift
@@ -0,0 +1,42 @@
+//
+//  MovieListViewController.swift
+//  Movie List
+//
+//  Created by Nelson Gonzalez on 1/11/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MovieListViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+
+    
+
+}
+
+extension MovieListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! MovieTableViewCell
+        
+        
+        
+        return cell
+    }
+    
+    
+}
