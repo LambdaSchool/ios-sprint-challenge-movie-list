03a119363460d20f6eef10c811cc09aad199df07
Vijay Das <vijaytdas@gmail.com>
2019-01-11 11:03:20 -0600

    create MovieModel, MovieModelController, MovieTableViewController files

---
+++ Movie List/Model Controllers/MovieModelController.swift
@@ -0,0 +1,9 @@
+//
+//  MovieModelController.swift
+//  Movie List
+//
+//  Created by Vijay Das on 1/11/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
---
+++ Movie List/Models/MovieModel.swift
@@ -0,0 +1,14 @@
+//
+//  MovieModel.swift
+//  Movie List
+//
+//  Created by Vijay Das on 1/11/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Movie {
+    var title: String
+
+}
