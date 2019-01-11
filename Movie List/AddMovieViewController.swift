d9f12fb2b780ee0001d1359e13388992eb9cae9f
Sean Doyle <sdoylelambda@gmail.com>
2019-01-11 11:21:41 -0500

    Basic storyboard and skeleton files connected.

---
+++ Movie List/AddMovieViewController.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+class AddMovieViewController: UIViewController {
+    
+    @IBOutlet weak var textFieldOutlet: UITextField!
+    
+    @IBOutlet weak var addMovieAction: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
