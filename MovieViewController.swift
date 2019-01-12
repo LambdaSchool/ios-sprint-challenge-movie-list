5e01a41823e0fd507a983fee82c026edbd795f96
Diante Lewis-Jolley <diantelewis-jolley@diantes-macbook-pro.local>
2019-01-11 18:32:18 -0600

    fixed initial  view and added a tableViewController

--- MovieViewController.swift
+++ MovieViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class MovieViewController: UIViewController, UITableViewDataSource {
+class MovieViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var textField: UITextField!
@@ -18,38 +18,14 @@ class MovieViewController: UIViewController, UITableViewDataSource {
         MovieController.shared.addMovie(movie)
         tableView.reloadData()
     }
-
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
     
 
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return MovieController.shared.movies.count
-    }
-
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath)
-
-        // Configure the cell...
-        let movies = MovieController.shared.movies
-        let movie = movies[indexPath.row]
-        cell.textLabel!.text = movie.title
-
-        return cell
-    }
-
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        guard editingStyle == .delete else { return }
-
-        let movie = MovieController.shared.movies[indexPath.row]
-        MovieController.shared.removeMovies(movie)
-    }
+   
 }
 
 
