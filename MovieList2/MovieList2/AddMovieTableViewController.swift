b5d232ea70707755e5e14795615bffcff706f14e
Diante Lewis-Jolley <diantelewis-jolley@diantes-macbook-pro.local>
2019-01-18 12:43:52 -0600

    Finished product with add movies and delete functions

---
+++ MovieList2/MovieList2/AddMovieTableViewController.swift
@@ -0,0 +1,23 @@
+//
+//  AddMovieTableViewController.swift
+//  MovieList2
+//
+//  Created by Diante Lewis-Jolley on 1/18/19.
+//  Copyright © 2019 Diante Lewis-Jolley. All rights reserved.
+//
+
+import UIKit
+
+class AddMovieTableViewController: UITableViewController {
+
+
+    @IBOutlet weak var textField: UITextField!
+
+
+    @IBAction func addButton(_ sender: Any) {
+        guard let movie = textField.text else { return }
+        MovieController.shared.addMovie(movie)
+        textField.text = nil
+        tableView.reloadData()
+    }
+}
