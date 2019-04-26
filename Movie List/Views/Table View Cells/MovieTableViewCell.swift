72d50ced3f9c69ba7b20a524cc5e70013121b03d
Michael R.F <mrflowers22@uwalumni.com>
2019-04-26 12:04:04 -0400

    implemented the delegate protocol in the table View cell file.

--- Movie List/Views/Table View Cells/MovieTableViewCell.swift
+++ Movie List/Views/Table View Cells/MovieTableViewCell.swift
@@ -8,13 +8,33 @@
 
 import UIKit
 
+protocol MovieTableViewCellDelegate: class {
+    func tappedSeenButton(on cell: MovieTableViewCell)
+}
+
 class MovieTableViewCell: UITableViewCell {
 
     var movie: Movie?
+    weak var delegate: MovieTableViewCellDelegate?
     
     @IBOutlet weak var movieTitleLabel: UILabel!
     @IBOutlet weak var seenButtonProperties: UIButton!
 
     @IBAction func seenButtonPressed(_ sender: UIButton) {
+        //alert our delegate that somethng has happened. i.e. this button was pressed.
+        delegate?.tappedSeenButton(on: self)
+    }
+    
+    private func updateViews(){
+        
+        //check to see if its a legit movie
+        guard let passedInMovie = movie else { return }
+        
+        //set the text of the label to reflect to name of the movie
+        movieTitleLabel.text = passedInMovie.title
+        
+        //set the button title based on the boolean value of hasSeen
+        let seenButtonTitle = passedInMovie.hasSeen ? "Seen" : "Not Seen"
+        seenButtonProperties.setTitle(seenButtonTitle, for: .normal)
     }
 }
