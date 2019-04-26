a1585ff1da97ee691dda7f297999f97c3a7f4602
Sameera Roussi <slr@Sameeras-MacBook-Pro.local>
2019-04-26 11:23:08 -0500

    Created add, movie, delete, move functions

---
+++ Movie List/Controllers/AddMoviesViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AddMoviesViewController.swift
+//  Movie List
+//
+//  Created by Sameera Roussi on 4/26/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class AddMoviesViewController: UIViewController {
+
+    @IBOutlet weak var movieTitleTextField: UITextField!
+    
+    @IBAction func addMovieButtonTapped(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
