566431c352cdef5d14ff48937a4b5f402c402dd1
jayfenam <jonathanferrer95@gmail.com>
2019-04-26 12:24:40 -0400

    Added all the UI elements to the storyboards, linked them all together using segues and references.  Created the model

---
+++ iosSprintChallenge1/iosSprintChallenge1/ViewControllers/AddNewMovieViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddNewMovieViewController.swift
+//  iosSprintChallenge1
+//
+//  Created by Jonathan Ferrer on 4/26/19.
+//  Copyright © 2019 Jonathan Ferrer. All rights reserved.
+//
+
+import UIKit
+
+class AddNewMovieViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ iosSprintChallenge1/iosSprintChallenge1/ViewControllers/MoviesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MoviesTableViewCell.swift
+//  iosSprintChallenge1
+//
+//  Created by Jonathan Ferrer on 4/26/19.
+//  Copyright © 2019 Jonathan Ferrer. All rights reserved.
+//
+
+import UIKit
+
+class MoviesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
