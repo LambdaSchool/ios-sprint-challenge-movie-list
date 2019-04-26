9edf9b2305866e4345cf95cbb9d33f411fd6b47b
blairholmgren <blair.holmgren@gmail.com>
2019-04-26 10:02:56 -0700

    Added movieTabBarController.swift file, filled in tableViewController function declarations.

---
+++ iOS-Sprint-Challenge/iOS-Sprint-Challenge/MoviesTabBarViewController.swift
@@ -0,0 +1,38 @@
+//
+//  MoviesTabBarViewController.swift
+//  iOS-Sprint-Challenge
+//
+//  Created by Blair Holmgren on 4/26/19.
+//  Copyright © 2019 Blair Holmgren. All rights reserved.
+//
+
+import UIKit
+
+class MoviesTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        passMovieControllerToChildViewControllers()
+        // Do any additional setup after loading the view.
+    }
+    
+    func passMovieControllerToChildViewControllers() {
+        for childViewController in children {
+            guard let childViewController = childViewController as? MovieController else { return }
+            childViewController.movieController = movieController
+        }
+        
+    
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    let movieController = MovieController()
+}
--- iOS-Sprint-Challenge/iOS-Sprint-Challenge/MoviesTableViewController.swift
+++ iOS-Sprint-Challenge/iOS-Sprint-Challenge/MoviesTableViewController.swift
@@ -8,34 +8,28 @@
 
 import UIKit
 
-class MoviesTableViewController: UITableViewController {
+class MoviesTableViewController: UITableViewController, MovieControllerProtocol {
     
+    var movieController: MovieController?
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-         //self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
-    
-
-   
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        
+        return movieController?.movies.count ?? 0
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MoviesCell", for: indexPath)
 
-        // Configure the cell...
+        let movie = movieController?.movies[indexPath.row]
+        cell.textLabel?.text = movie?.name
+    
 
         return cell
     }
