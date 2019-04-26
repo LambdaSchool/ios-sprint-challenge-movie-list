b9232cca60dbf2ffcf44b261621d01d15793fa8d
Michael R.F <mrflowers22@uwalumni.com>
2019-04-26 11:27:47 -0400

    set up tab bar controller, a view controller and a table view controller. and added applicable source code files.

---
+++ Movie List/AddMovieViewController.swift
@@ -0,0 +1,33 @@
+//
+//  AddMovieViewController.swift
+//  Movie List
+//
+//  Created by Michael Flowers on 4/26/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class AddMovieViewController: UIViewController {
+
+    @IBOutlet weak var addMovieTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func addMovieButtonPressed(_ sender: UIButton) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Movie List/MovieTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MovieTableViewCell.swift
+//  Movie List
+//
+//  Created by Michael Flowers on 4/26/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var movieTitleLabel: UILabel!
+    @IBOutlet weak var seenButtonProperties: UIButton!
+
+    @IBAction func seenButtonPressed(_ sender: UIButton) {
+    }
+}
