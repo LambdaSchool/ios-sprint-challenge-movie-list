a24856ce23d6261b932743fa145d88c620e7b62d
Christopher Aronson <christopheraronson@icloud.com>
2019-04-26 10:58:42 -0700

    Added MovieTableViewCell Class. Implemented numberOfCells, editingStyle, and numberOfRowsInSection functions

--- Movie List/MovieTableViewController.swift
+++ Movie List/MovieTableViewController.swift
@@ -11,6 +11,10 @@ import UIKit
 class MovieTableViewController: UITableViewController, MoviesPresenter {
     var movieController: MovieController?
     
+    
+
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,26 +27,25 @@ class MovieTableViewController: UITableViewController, MoviesPresenter {
     }
 
     // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        // #warning Incomplete implementation, return the number of sections
+        guard let numberOfCells = movieController?.movies.count else { return 0 }
+        
+        return numberOfCells
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath)
 
-        // Configure the cell...
-
+        guard let movieCell = cell as? MovieTableViewCell else { return cell }
+        
+        let movie = movieController?.movies[indexPath.row]
+        movieCell.movieTitleLabel.text = movie?.movieTitle
+        
         return cell
     }
-    */
+
 
     /*
     // Override to support conditional editing of the table view.
@@ -52,17 +55,17 @@ class MovieTableViewController: UITableViewController, MoviesPresenter {
     }
     */
 
-    /*
+
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+
+            guard let movieToDelete = movieController?.movies[indexPath.row] else { return }
+            movieController?.deleteMovie(movieToBeDeleted: movieToDelete)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
 
     /*
     // Override to support rearranging the table view.
