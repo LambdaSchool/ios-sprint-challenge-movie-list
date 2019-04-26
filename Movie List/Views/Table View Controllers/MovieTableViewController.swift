f36caae93d555a30d2220914d22de3053c130f0a
Michael R.F <mrflowers22@uwalumni.com>
2019-04-26 11:50:04 -0400

    created segue from view controller to table view controller.

--- Movie List/Views/Table View Controllers/MovieTableViewController.swift
+++ Movie List/Views/Table View Controllers/MovieTableViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class MovieTableViewController: UITableViewController {
 
+    var movieController: MovieController?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
--- Movie List/Views/View Controllers/AddMovieViewController.swift
+++ Movie List/Views/View Controllers/AddMovieViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class AddMovieViewController: UIViewController {
+    
+    var movieController = MovieController()
 
     @IBOutlet weak var addMovieTextField: UITextField!
     override func viewDidLoad() {
@@ -18,16 +20,30 @@ class AddMovieViewController: UIViewController {
     }
     
     @IBAction func addMovieButtonPressed(_ sender: UIButton) {
+        //check to see if there is text in the text field
+        guard let movie = addMovieTextField.text, !movie.isEmpty else { return }
+        
+        //there is text so now we can create a movie
+        movieController.createMovie(name: movie)
+        
+        //now we can segue to the movie list scene.
+        performSegue(withIdentifier: "toMovieSegue", sender: nil)
     }
     
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        
+        //pass the movieController over to the movie list scene
+        if segue.identifier == "toMovieSegue" {
+            let movieVC = segue.destination as! MovieTableViewController
+            movieVC.movieController = movieController
+        }
     }
-    */
+    
 
 }
