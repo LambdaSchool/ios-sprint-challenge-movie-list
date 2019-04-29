84e2e0a10665ba6db2e2396265ab5b8bbd3a9136
Kobe McKee <kobe.mckee@gmail.com>
2019-04-28 19:56:53 -0600

    Found the issue with hasSeen button. Corrected it

--- Movie List/MovieController.swift
+++ Movie List/MovieController.swift
@@ -14,23 +14,25 @@ class MovieController {
     var movies: [Movie] = []
    
     func createMovie(title: String) {
+        //create newMovie & add it to array of movies
         let newMovie = Movie(title: title, hasSeen: false)
         movies.append(newMovie)
     }
     
     func deleteMovie(movie: Movie) {
+        // take an existing movie and remove it from the array of movies
         guard let movieIndex = movies.index(of: movie) else { return }
         movies.remove(at: movieIndex)
     }
     
     func toggleHasSeen(movie: Movie) {
+        // update movie to hasSeen or hasNotSeen
         var updatedMovie = movie
         updatedMovie.hasSeen = !updatedMovie.hasSeen
         
-        if let movieIndex = movies.index(of: movie) {
-            movies[movieIndex] = movie
-        }
-
+        // replace the oldMovie of the same index with the updatedMovie
+        guard let movieIndex = movies.index(of: movie) else { return }
+        movies[movieIndex] = updatedMovie
     }
     
     
