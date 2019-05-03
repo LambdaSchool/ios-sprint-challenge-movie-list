7e4e68dc2da3bf67ff98b5de6693049435f3415a
Hayden Hastings <haydenhastings32@gmail.com>
2019-05-03 10:45:45 -0600

    Added code for MovieTableViewController. Filled in cellForRowAt, EditingStyle, prepareForSegue, and Added a viewWillAppear with code.

--- Movie List/Model/ViewControllers/MovieTableViewController.swift
+++ Movie List/Model/ViewControllers/MovieTableViewController.swift
@@ -10,35 +10,48 @@ import UIKit
 
 class MovieTableViewController: UITableViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-    tableView.reloadData()
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
     }
 
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return movieController.movies.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as? MovieTableViewCell else { return UITableViewCell() }
+        
+        let movie = movieController.movies[indexPath.row]
+        cell.movie = movie
+        
+        return cell
     }
 
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            
+            let movie = movieController.movies[indexPath.row]
+            movieController.delete(movie: movie)
+            
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
 
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "ToAddMovie" {
+            guard let toAddMovieVC = segue.destination as? AddMovieViewController else { return }
+            
+            toAddMovieVC.movieController = movieController
+        }
     }
     
+    var movieController = MovieController()
 }
