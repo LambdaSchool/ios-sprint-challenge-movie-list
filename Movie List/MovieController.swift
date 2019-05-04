b22979e2e110f1e8dcae7a59f05b1ab79d2aa84e
Dongwoo Pae <dongwoopae@gmail.com>
2019-05-04 10:04:40 -0700

    set up model and model controller

---
+++ Movie List/MovieController.swift
@@ -0,0 +1,22 @@
+//
+//  MovieController.swift
+//  Movie List
+//
+//  Created by Dongwoo Pae on 5/4/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class MovieController {
+    var names: [Movie] = []
+    
+    func addMovieName(name: String, seen: Bool) {
+        let addMovie = Movie.init(name: name, seen: seen)
+        names.append(addMovie)
+    }
+    
+    func toggleSeen(for movie: Movie) {
+        movie.seen = !movie.seen
+    }
+}
