b02fa3b654c92ea80fa491f9516a015c839a7d37
Nate Hedgeman <nate.hedgeman@gmail.com>
2019-05-04 12:48:45 -0400

    Added UI elements, View Controllers, Navigation Controller,text field, button

---
+++ Movie List/Movie.swift
@@ -0,0 +1,14 @@
+//
+//  Movie.swift
+//  Movie List
+//
+//  Created by Nathan Hedgeman on 5/4/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Movie {
+    var title: String
+    var isSeen: Bool = false
+}
