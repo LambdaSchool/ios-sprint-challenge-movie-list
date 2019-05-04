7185c79c8a8b45ce5654c94ffcd62b049c4ef5a4
Dongwoo Pae <dongwoopae@gmail.com>
2019-05-04 09:42:33 -0700

    view set up completed

---
+++ Movie List/AddNewMovieListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddNewMovieListViewController.swift
+//  Movie List
+//
+//  Created by Dongwoo Pae on 5/4/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class AddNewMovieListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
