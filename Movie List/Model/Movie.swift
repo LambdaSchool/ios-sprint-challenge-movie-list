e0b60964a34adf95415dc817ee23170d975fb675
Kat Milton <mltnkat@gmail.com>
2019-06-07 08:46:46 -0700

    Initial commit. Storyboard set up.

---
+++ Movie List/Model/Movie.swift
@@ -0,0 +1,22 @@
+//
+//  Movie.swift
+//  Movie List
+//
+//  Created by Kat Milton on 6/7/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+/*
+ The project calls for a Movie model object.
+ It should contain properties for the movie's name and whether or not it has been seen.
+
+ */
+
+struct Movie: Equatable {
+    var name: String
+    var seen: Bool = false
+    
+    
+}
