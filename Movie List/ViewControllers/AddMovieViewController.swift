ef87b492f682b06cfc7c628bda651cd1383845d2
Marlon Raskin <marlonjraskin@gmail.com>
2019-06-07 12:09:48 -0400

    Added IBOutlets and IBAction for textField and button on AddMovieViewController.

---
+++ Movie List/ViewControllers/AddMovieViewController.swift
@@ -0,0 +1,33 @@
+//
+//  AddMovieViewController.swift
+//  Movie List
+//
+//  Created by Marlon Raskin on 6/7/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+
+
+
+class AddMovieViewController: UIViewController {
+
+	
+	@IBOutlet var addMovieTextField: UITextField!
+	
+	
+	
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+	@IBAction func addMovieButtonTapped(_ sender: UIButton) {
+		
+	}
+	
+
+}
--- Movie List/ViewControllers/MovieTableTableViewController.swift
+++ Movie List/ViewControllers/MovieTableTableViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class MovieTableTableViewController: UITableViewController {
 
+	var movieController = MovieController()
+	
+	
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,14 +25,9 @@ class MovieTableTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return movieController.movies.count
     }
 
     /*
@@ -42,13 +40,6 @@ class MovieTableTableViewController: UITableViewController {
     }
     */
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
 
     /*
     // Override to support editing the table view.
@@ -62,20 +53,6 @@ class MovieTableTableViewController: UITableViewController {
     }
     */
 
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
 
     /*
     // MARK: - Navigation
