72587fcb5681032f22fda92564bd3b784a05be67
Ife Banire <IfeBanire@yahoo.com>
2019-06-07 14:45:36 -0400

    Updated project

---
+++ Movie List/Movie.swift
@@ -0,0 +1,20 @@
+//
+//  Movie.swift
+//  Movie List
+//
+//  Created by Lambda_School_Loaner_101 on 6/7/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+
+struct Movie {
+    var moviesName: String
+    var isSeen: Bool
+    
+    init(moviesName: String, isSeen: Bool = false) {
+        self.moviesName = moviesName
+        self.isSeen = isSeen
+    }
+}
---
+++ Movie List/MovieController.swift
@@ -0,0 +1,25 @@
+//
+//  MovieController.swift
+//  Movie List
+//
+//  Created by Lambda_School_Loaner_101 on 6/7/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class MovieController {
+    
+   var movies: [Movie] = []
+//
+//    func createMovies() {
+//        let newMovies = movies
+//        movies.append(<#T##newElement: Movie##Movie#>)
+//
+//    }
+//
+//    func deleteMovies() {
+//        movies.remove(at: <#T##Int#>)
+//    }
+//
+}
