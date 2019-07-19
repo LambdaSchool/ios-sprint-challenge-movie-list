e553c7caf943bdb77db017da6412bbfd81888d9d
bradleyyin <bradleyyin@gmail.com>
2019-07-19 08:27:52 -0700

    UI set up

---
+++ MovieList/AddMovieViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AddMovieViewController.swift
+//  MovieList
+//
+//  Created by Bradley Yin on 7/19/19.
+//  Copyright © 2019 bradleyyin. All rights reserved.
+//
+
+import UIKit
+
+class AddMovieViewController: UIViewController {
+
+    @IBOutlet weak var movieTextfield: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func addMovieTapped(_ sender: Any) {
+    }
+    
+
+}
