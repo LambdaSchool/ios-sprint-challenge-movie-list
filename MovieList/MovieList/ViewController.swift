a33257d64117244038a1420cd331c0e71981e863
bfvilchez <bfvilchez@icloud.com>
2019-07-19 09:03:15 -0600

    created a new movieList  xode project

---
+++ MovieList/MovieList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieList
+//
+//  Created by brian vilchez on 7/19/19.
+//  Copyright © 2019 brian vilchez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
