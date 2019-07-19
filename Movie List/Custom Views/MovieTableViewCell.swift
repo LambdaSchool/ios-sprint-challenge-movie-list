e0cf7fa51a7c484496d10ec0949616c25ef6b3b3
Andrew Ruiz <nukedoom@gmail.com>
2019-07-19 08:59:50 -0700

    Added movie view controller and detail view controller

---
+++ Movie List/Custom Views/MovieTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  MovieTableViewCell.swift
+//  Movie List
+//
+//  Created by Andrew Ruiz on 7/19/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var movieName: UILabel!
+    @IBOutlet weak var isSeen: UILabel!
+    
+    
+  
+
+}
