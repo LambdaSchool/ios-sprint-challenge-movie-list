a65c34b441f31936f8c48046e222301c8ae8abb8
JerryHaaser <jerry.haaser@hotmail.com>
2019-07-19 11:40:12 -0500

    Deleted and created new View  to match directions, added label and button in cell

---
+++ MovieListSprint/MovieListSprint/MovieListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieListTableViewCell.swift
+//  MovieListSprint
+//
+//  Created by admin on 7/19/19.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import UIKit
+
+class MovieListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
