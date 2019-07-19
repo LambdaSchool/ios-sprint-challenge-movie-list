5f57e0a31013636a11793b4db66ade185d869856
Percy Ngan <percival.ngan@gmail.com>
2019-07-19 12:59:36 -0500

    Finished UI

---
+++ Movie List/AddMovieViewController.swift
@@ -0,0 +1,27 @@
+//
+//  AddMovieViewController.swift
+//  Movie List
+//
+//  Created by Percy Ngan on 7/19/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class AddMovieViewController: UIViewController {
+    
+    @IBOutlet weak var movieTextField: UITextField!
+    
+
+    @IBAction func addMovieTapped(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+ 
+
+}
---
+++ Movie List/MovieListViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MovieListViewController.swift
+//  Movie List
+//
+//  Created by Percy Ngan on 7/19/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MovieListViewController: UIViewController {
+    
+    @IBOutlet weak var movieLabel: UILabel!
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+ 
+
+}
