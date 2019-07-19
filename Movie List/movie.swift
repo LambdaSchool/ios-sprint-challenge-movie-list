66e89bc8a88bfc8e4c5d9e982cb884bba4241485
ry <xxsnailyxx@gmai.com>
2019-07-19 16:30:34 -0700

    Connected add button and added functions to hopefully save input

--- Movie List/movie.swift
+++ Movie List/movie.swift
@@ -12,3 +12,34 @@ struct Movie {
     var name: String
     var seen: Bool
 }
+class Model {
+    static let shared = Model()
+    private init() {}
+    private var movies: [String] = []
+    
+    func addMovie(_ movie: String) {
+        movies.append(movie)
+
+    }
+    
+    func removeMovie(at index: Int) {
+        movies.remove(at: index)
+    }
+    
+    func moveMovie(from index: Int, to destinationIndex: Int) {
+        let movie = movies.remove(at: index)
+        movies.insert(movie, at: destinationIndex)
+
+    }
+    
+    func movieCount() -> Int {
+        return movies.count
+    }
+    
+    func movie(at index: Int) -> String {
+        return movies[index]
+    }
+    
+  
+   
+    }
