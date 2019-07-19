5601d4a8be5a4a4b25a80a4b47668c6518717fa2
ry <xxsnailyxx@gmai.com>
2019-07-19 10:55:58 -0700

    working so far

--- Movie List/ViewController.swift
+++ Movie List/ViewController.swift
@@ -8,3 +8,30 @@
 
 import UIKit
 
+class ViewController: UIViewController {
+    
+    var movies = [Movie]() // data source
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        movies = []
+    }
+    
+    
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FishCell", for: indexPath)
+        let newMovie = movies[indexPath.row]
+        cell.textLabel?.text = newMovie.name
+        
+        return cell
+    }
+    
+    
+}
---
+++ Movie List/movie.swift
@@ -0,0 +1,14 @@
+//
+//  movie.swift
+//  Movie List
+//
+//  Created by Ry Bautista on 7/19/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Movie {
+    var name: String
+    var seen: Bool
+}
