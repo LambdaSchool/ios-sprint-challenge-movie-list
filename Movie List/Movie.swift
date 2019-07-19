2f3b6077bef5619b8063f7ad7ff1979a75169f4a
Nicolas Rios <nickrios33@yahoo.com>
2019-07-19 10:53:17 -0700

    updated file for todays challenge

---
+++ Movie List/Movie.swift
@@ -0,0 +1,16 @@
+//
+//  Movie.swift
+//  Movie List
+//
+//  Created by Nicolas Rios on 7/19/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct  AMovie {
+    let TheMovieName: String
+    var HasBeenSeen: Bool
+    var HasnotBeenSeen: Bool
+}
+
