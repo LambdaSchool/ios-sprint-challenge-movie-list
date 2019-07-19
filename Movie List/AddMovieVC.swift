3590d166f0bd0eac8977d22bc1bffafc7bb52663
Jeffrey Santana <SantanaJny@gmail.com>
2019-07-19 11:59:02 -0400

    Initial commit

---
+++ Movie List/AddMovieVC.swift
@@ -0,0 +1,24 @@
+//
+//  AddMovieVC.swift
+//  Movie List
+//
+//  Created by Jeffrey Santana on 7/19/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class AddMovieVC: UIViewController {
+
+	@IBOutlet weak var titleTextfield: UITextField!
+	
+	override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+	
+	@IBAction func addMovieBtnAction(_ sender: Any) {
+	}
+	
+}
---
+++ Movie List/MovieCell.swift
@@ -0,0 +1,17 @@
+//
+//  MovieCell.swift
+//  Movie List
+//
+//  Created by Jeffrey Santana on 7/19/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+	@IBOutlet weak var titleLbl: UILabel!
+	
+	@IBAction func SeenBtnAction(_ sender: UIButton) {
+	}
+}
