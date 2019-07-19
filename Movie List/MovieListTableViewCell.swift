3af06de29fe8129ba90c2e94381f38ef090ed8fb
ryandutson <mr.dutson@gmail.com>
2019-07-19 11:30:57 -0600

    Initial storyboard elements added.

---
+++ Movie List/MovieListTableViewCell.swift
@@ -0,0 +1,10 @@
+//
+//  MovieListTableViewCell.swift
+//  Movie List
+//
+//  Created by Ryan Dutson on 7/19/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
