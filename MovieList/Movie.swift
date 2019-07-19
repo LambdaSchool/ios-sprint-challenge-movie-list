860ee3a0d6cb93ddf19c4cc89fea22e16d6ab763
bradleyyin <bradleyyin@gmail.com>
2019-07-19 08:29:07 -0700

    set up model

---
+++ MovieList/Movie.swift
@@ -0,0 +1,13 @@
+//
+//  Movie.swift
+//  MovieList
+//
+//  Created by Bradley Yin on 7/19/19.
+//  Copyright © 2019 bradleyyin. All rights reserved.
+//
+
+import Foundation
+struct Movie {
+    let name : String
+    var haveSeen : Bool
+}
