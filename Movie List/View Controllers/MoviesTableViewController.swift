0f3c29bfd22e3cc1648762008c738bdedb2feb14
Andrew Ruiz <nukedoom@gmail.com>
2019-07-19 08:41:46 -0700

    Added MoviesViewController, tableview, and connected them to the Main.storyboard

---
+++ Movie List/View Controllers/MoviesTableViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MoviesTableViewController.swift
+//  Movie List
+//
+//  Created by Andrew Ruiz on 7/19/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MoviesTableViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+
+}
