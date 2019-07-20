1a8aba0b54482bb0df72dfb97a1041aece965ae1
Nicolas Rios <nickrios33@yahoo.com>
2019-07-20 02:14:06 -0700

    added button to project

---
+++ Movie List/UIViewController.swift
@@ -0,0 +1,21 @@
+//
+//  UIViewController.swift
+//  Movie List
+//
+//  Created by Nicolas Rios on 7/19/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class UIViewController: UITableView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
