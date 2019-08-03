99fe8cc3a8670614d3abceb8cc07815d9f35c661
KALISA FISHER <kiecreations@gmail.com>
2019-08-03 13:35:02 -0400

    Save Changes

---
+++ MovieList/MovieList/Models/MovieList.swift
@@ -0,0 +1,9 @@
+//
+//  MovieList.swift
+//  MovieList
+//
+//  Created by Lisa Fisher on 8/3/19.
+//  Copyright © 2019 Lisa Fisher. All rights reserved.
+//
+
+import Foundation
---
+++ MovieList/MovieList/View Controllers/AddMovieViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddMovieViewController.swift
+//  MovieList
+//
+//  Created by Lisa Fisher on 8/3/19.
+//  Copyright © 2019 Lisa Fisher. All rights reserved.
+//
+
+import UIKit
+
+class AddMovieViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
