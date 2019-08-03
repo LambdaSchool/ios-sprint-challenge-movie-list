4ae250ce4b4fadd2894f487428d5431c2105326a
KALISA FISHER <kiecreations@gmail.com>
2019-08-03 12:08:27 -0400

    Create Movie List Project

---
+++ MovieList/MovieList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieList
+//
+//  Created by Lisa Fisher on 8/3/19.
+//  Copyright © 2019 Lisa Fisher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
