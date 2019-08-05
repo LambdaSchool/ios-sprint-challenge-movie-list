785d21ec82bb16b1c1def111d8152f3cd0c4996d
Nicolas Rios <nickrios33@yahoo.com>
2019-08-04 18:19:58 -0700

    updated work on challenge

---
+++ Movie List/movie.swift
@@ -0,0 +1,20 @@
+//
+//  movie.swift
+//  Movie List
+//
+//  Created by Nicolas Rios on 7/31/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+struct Movie {
+    var name: String
+    var hasSeen: bool
+    init (name: String ,hasSeen:bool = false) {
+        self.name
+        self.hasSeen
+    }
+
+}
+
+
