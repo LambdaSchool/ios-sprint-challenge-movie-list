e4e5b781a1803d0820ca5dd97c1b46f882db0b60
Jessie Griffin <i.jessie.me@icloud.com>
2019-08-04 21:53:55 -0400

    Added protocol function and datasource protocol to extensions in MovieTableViewController file

--- Movie List/View Controllers/MovieTableViewController.swift
+++ Movie List/View Controllers/MovieTableViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class MovieTableViewController: UIViewController {
 
+    
+    @IBOutlet weak var movieListTableView: UITableView!
+    
+    var movieList: [Movie] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,14 +22,41 @@ class MovieTableViewController: UIViewController {
     }
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
+        if segue.identifier == "AddMovieShowSegue" {
+            if let addMovieVC = segue.destination as? AddMovieViewController {
+                addMovieVC.delegate = self
+            }
+        }
+    }
+}
+
+extension MovieTableViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movieList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
+        
+        return cell
+    }
+    
+    
+}
+
+extension MovieTableViewController: AddNewMovie {
+    func newMovieWasAdded(_ movie: Movie) {
+        movieList.append(movie)
+        dismiss(animated: true, completion: nil)
+        movieListTableView.reloadData()
+    }
+    
+    
 }
