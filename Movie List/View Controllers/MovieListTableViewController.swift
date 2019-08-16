65c7f2519a17ec4c1d1ef968613e137c1c21bab8
ryandutson <mr.dutson@gmail.com>
2019-08-16 17:02:50 -0600

    Final commit, code completed, thanks for all your help, Mitch. I couldn't have done it without your help.

--- Movie List/View Controllers/MovieListTableViewController.swift
+++ Movie List/View Controllers/MovieListTableViewController.swift
@@ -8,35 +8,37 @@
 
 import UIKit
 
-class MovieListTableViewController: UIViewController {
+class MovieListTableViewController: UITableViewController {
 
     let movieModelController = MovieController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.reloadData()
         
         // Do any additional setup after loading the view.
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return movieModelController.movies.count
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as? MovieTableViewCell else { return UITableViewCell() }
         let movie = movieModelController.movies[indexPath.row]
         cell.movieTitleLabel.text = movie.movieName
         return cell
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "AddMovieShowSegue" {
+            let addMovieVC = segue.destination as? AddMovieViewController
+            addMovieVC?.movieController = movieModelController
+        }
     }
-    */
-
 }
