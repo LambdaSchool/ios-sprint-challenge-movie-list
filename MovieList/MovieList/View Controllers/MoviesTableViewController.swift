ee910a050a018e9c7f4a6272316686f63c035028
Joshua Franklin <jfranklin1607@gmail.com>
2019-08-16 16:52:44 -0400

    New Push

--- MovieList/MovieList/View Controllers/MoviesTableViewController.swift
+++ MovieList/MovieList/View Controllers/MoviesTableViewController.swift
@@ -33,15 +33,25 @@ class MoviesTableViewController: UITableViewController {
         return cell
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+   override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            movieController.movies.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
     }
-    */
+    
 
+    
+    // MARK: - Navigation
+    
+   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "AddMovieSegue" {
+          let addMovieVC = segue.destination as? AddMovieViewController
+            addMovieVC?.movieController = movieController
+        } else if  segue.identifier == "MovieDetailSegue" {
+            guard let indexPath = tableView.indexPathForSelectedRow else {return}
+            let movieDetailVC = segue.destination as? MovieDetailViewController
+            movieDetailVC?.movie = movieController.movies[indexPath.row]
+    }
+ }
 }
