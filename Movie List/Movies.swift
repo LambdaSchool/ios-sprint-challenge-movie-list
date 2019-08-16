b7e348f333247b2a439556721d6c3290c8ca2f4a
AustinPotts <pottsevolvements@gmail.com>
2019-08-16 11:28:12 -0400

    StoryBoard Complete

---
+++ Movie List/Movies.swift
@@ -0,0 +1,13 @@
+//
+//  Movies.swift
+//  Movie List
+//
+//  Created by Austin Potts on 8/16/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Movie {
+    let movieTitle: String
+}
