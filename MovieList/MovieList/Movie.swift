072bf1cb0fbb31470c72af6b6867262283129d28
Joshua Franklin <jfranklin1607@gmail.com>
2019-08-16 13:06:18 -0400

    MovieList
    first commit

---
+++ MovieList/MovieList/Movie.swift
@@ -0,0 +1,15 @@
+//
+//  Movie.swift
+//  MovieList
+//
+//  Created by Joshua Franklin on 8/16/19.
+//  Copyright © 2019 AMSU. All rights reserved.
+//
+
+import Foundation
+
+struct Movie {
+    
+    let title: String
+    
+}
---
+++ MovieList/MovieList/MovieListViewController.swift
@@ -0,0 +1,49 @@
+//
+//  MovieListViewController.swift
+//  MovieList
+//
+//  Created by Joshua Franklin on 8/16/19.
+//  Copyright © 2019 AMSU. All rights reserved.
+//
+
+import UIKit
+
+class MovieListViewController: UIViewController {
+
+    @IBOutlet var tableView: UITableView!
+    
+    var movies: [Movie] = []
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension MovieListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+    
+}
