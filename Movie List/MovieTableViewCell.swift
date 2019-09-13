181eeb9d78a3fe33a186d4a11a141479144dd8e9
Wilma <kima_92@fastmail.com>
2019-09-13 12:28:34 -0400

    Added Stack View with TextField and Button to new View Controller in StoryBoard

--- Movie List/MovieTableViewCell.swift
+++ Movie List/MovieTableViewCell.swift
@@ -13,9 +13,11 @@ class MovieTableViewCell: UITableViewCell {
     @IBOutlet weak var movieTextField: UITextField!
     
     
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        //seenButtonPressed.title
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -24,6 +26,21 @@ class MovieTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
 
+    
+    // TODO : - Configure the Seen / Not Seen Button
+    
     @IBAction func seenButtonPressed(_ sender: Any) {
+//        if let buttonTitle = (sender as AnyObject).currentTitle {
+//            buttonTitle.title
+//            
+//             //buttonTitle.setTitle("Not Seen",for: .normal)
+//            
+//            
+//            
+//           // buttonTitle?.titl = "Seen"
+//           // brain?.setOperator(buttonTitle)
+            
+        }
+        
     }
 }
