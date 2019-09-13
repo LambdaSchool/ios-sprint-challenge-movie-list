8807e43e7a38d1d55608c6c52d8c95c5b157a21b
Gi Pyo Kim <gipyok@gmail.com>
2019-09-13 08:33:10 -0700

    Added UI and wired up outlets and actions

---
+++ AddMovieViewController.swift
@@ -0,0 +1,33 @@
+//
+//  AddMovieViewController.swift
+//  Movie List
+//
+//  Created by Gi Pyo Kim on 9/13/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class AddMovieViewController: UIViewController {
+    @IBOutlet weak var addMovieTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func addMovieTabbed(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Movie List/MovieListViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MovieListViewController.swift
+//  Movie List
+//
+//  Created by Gi Pyo Kim on 9/13/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MovieListViewController: UIViewController {
+    @IBOutlet weak var movieLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func hasBeenSeenTabbed(_ sender: UIButton) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
