865eeb01ffa4718a90acad2141fc9e51b75074ff
Wilma <kima_92@fastmail.com>
2019-09-13 11:23:25 -0400

    Added MoviesTableViewController

---
+++ Movie List/Movie.swift
@@ -0,0 +1,16 @@
+//
+//  Movie.swift
+//  Movie List
+//
+//  Created by macbook on 9/13/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+
+struct Movie {
+    var name: String
+    var hasBeenSeen: Bool = false
+}
+
