e2111854785fd601cc455f2f1af366c84cee9a06
Gavin Murphy <gavincmurphy@gmail.com>
2019-09-27 19:05:56 -0700

    Named MovieListViewController and deleted code lines

---
+++ Movie List/MovieListTableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MovieListTableViewController.swift
+//  Movie List
+//
+//  Created by Gavin Murphy on 9/27/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MovieListTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
