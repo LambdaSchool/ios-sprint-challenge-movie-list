d4d104194c44619f7051e68eae08e7f275820f14
Denis Cedeno <denis.cedeno@gmail.com>
2019-09-28 15:20:59 -0400

    working on addView

--- Movie List/addMovieViewController.swift
+++ Movie List/addMovieViewController.swift
@@ -7,24 +7,21 @@
 //
 
 import UIKit
-
+protocol addMovieDelegate{
+    func movieWasAdded(_ movie: Movie)
+}
 class addMovieViewController: UIViewController {
 
+    @IBOutlet weak var addMovieTextField: UITextField!
+    @IBOutlet weak var addMovieButton: UIButton!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+       
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+ 
 
 }
