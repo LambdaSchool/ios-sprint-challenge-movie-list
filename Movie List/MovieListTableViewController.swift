c2f5b46ce7eca36584f242ef39532c3aad9ffd92
Gavin Murphy <gavincmurphy@gmail.com>
2019-09-27 20:51:29 -0700

    Added an extension to the MovieListTableViewController

--- Movie List/MovieListTableViewController.swift
+++ Movie List/MovieListTableViewController.swift
@@ -8,12 +8,27 @@
 
 import UIKit
 
-class MovieListTableViewController: UITableViewController {
+class MovieListTableViewController: UIViewController {
+    
+    var movies: [Movie] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
 
+}
 
+extension MovieListTableViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    guard let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as? MovieTableViewCell else {
+        fatalError()
+        }
+        return cell
+    }
 }
