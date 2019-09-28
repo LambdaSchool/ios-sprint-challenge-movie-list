6918f163f3fcf569d38749087e171aa201e871bd
Alexander Thompson <53663334+alext8900@users.noreply.github.com>
2019-09-28 14:04:33 -0500

    Add files via upload

---
+++ ios-sprint-challenge-movie-list-master/MovieTitleTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieTitleTableViewCell.swift
+//  Movie List
+//
+//  Created by Alex Thompson on 9/28/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MovieTitleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
