dd2867d9b3f4f210b1b31052be60a84a0448aa59
Pravin Patel <patelpra337@gmail.com>
2019-09-29 16:14:57 -0500

    Add movieviewcontroller and configuring the file

---
+++ Movie List/MovieViewController.swift
@@ -0,0 +1,37 @@
+//
+//  MovieViewController.swift
+//  Movie List
+//
+//  Created by patelpra on 9/29/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MovieViewController: UITableViewController {
+    
+    let movieController = MovieController()
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ToAddMovie" {
+            guard let addMovieViewController = segue.destination as? AddMovieViewController else {
+                    return }
+            
+            addMovieViewController.movieController = self.movieController
+        }
+    }
+}
+
+
