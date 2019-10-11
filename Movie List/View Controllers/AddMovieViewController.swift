2a0a8ac0fd9115422463e95684445757ffe01608
Brandi <blubrandi0@gmail.com>
2019-10-11 11:12:35 -0600

    Added protocol to AddMovieViewController.

--- Movie List/View Controllers/AddMovieViewController.swift
+++ Movie List/View Controllers/AddMovieViewController.swift
@@ -8,6 +8,10 @@
 
 import UIKit
 
+protocol AddMovieDelegate {
+    func movieWasAdded(_ movie: Movie)
+}
+
 class AddMovieViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -17,14 +21,6 @@ class AddMovieViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
