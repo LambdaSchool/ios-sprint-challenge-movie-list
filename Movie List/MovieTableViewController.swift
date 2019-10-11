3212e1e6efc94bbe757e9ef2e4798e60a1a6d4e3
Dan M <djmthedev@gmail.com>
2019-10-11 09:20:21 -0700

    made MovieTableViewController conform to TableViewDataSource

--- Movie List/MovieTableViewController.swift
+++ Movie List/MovieTableViewController.swift
@@ -10,14 +10,18 @@ import UIKit
 
 class MovieTableViewController: UIViewController {
     
+    @IBOutlet weak var tableView: UITableView!
+    
     struct PropertyKeys {
         static let MovieCell = "MovieCell"
     }
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
     }
     
 
@@ -31,4 +35,19 @@ class MovieTableViewController: UIViewController {
     }
     */
 
+}
+
+extension MovieTableViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: PropertyKeys.MovieCell, for: indexPath) as? MovieTableViewCell else { return UITableViewCell() }
+        
+        let movie = movies[indexPath.row]
+        cell.movie = movie
+        return cell
+    }
 }
