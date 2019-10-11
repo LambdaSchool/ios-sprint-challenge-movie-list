08489c9880abf87c90666c685e6b73159163f792
Pioneer Pass <pioneerpass@protonmail.com>
2019-10-11 08:29:24 -0700

    GRINDING

---
+++ Movie List/Models/Movie.swift
@@ -0,0 +1,14 @@
+//
+//  Movie.swift
+//  Movie List
+//
+//  Created by Thomas Bernard Leonard II on 10/11/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Movie {
+    var name: String
+    var seen: Bool
+}
---
+++ Movie List/View Controllers/MovieTableViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MovieTableViewController.swift
+//  Movie List
+//
+//  Created by Thomas Bernard Leonard II on 10/11/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}
