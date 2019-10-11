f5d68b5173f435786597da285857c50db9c14f83
Skyler Suzuki <skylersuzuk1@gmail.com>
2019-10-11 08:34:05 -0700

    Baseline for movie list sprint challenge

---
+++ Movie List/Movie.swift
@@ -0,0 +1,14 @@
+//
+//  Movie.swift
+//  Movie List
+//
+//  Created by Lambda_School_Loaner_204 on 10/11/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Movie {
+    let name: String
+    var hasSeen: Bool
+}
