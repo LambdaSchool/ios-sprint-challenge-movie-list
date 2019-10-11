2f1d2bf52d88094529ec4d1cf5f6cc563bc0bf68
Pioneer Pass <pioneerpass@protonmail.com>
2019-10-11 08:50:46 -0700

    GRIND 2

--- Movie List/View Controllers/AddMovieViewController.swift
+++ Movie List/View Controllers/AddMovieViewController.swift
@@ -17,14 +17,8 @@ class AddMovieViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func cancelTapped(_ sender: UIBarButtonItem) {
+    dismiss(animated: true, completion: nil)
     }
-    */
 
 }
