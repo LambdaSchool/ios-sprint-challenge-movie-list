fd2ee24f9aa6916bc77e0ae9603c7d3690d451d5
Rwoltx <richardx1016@gmail.com>
2019-10-11 12:13:34 -0400

    Buttons and TextFields connected

---
+++ Movie List/Controllers/AddMovieViewController.swift
@@ -0,0 +1,41 @@
+//
+//  AddMovieViewController.swift
+//  Movie List
+//
+//  Created by Rick Wolter on 10/11/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class AddMovieViewController: UIViewController {
+
+    
+    
+    
+    @IBAction func addMovieTextField(_ sender: UITextField) {
+    }
+    
+    @IBAction func addMovieButtonClicked(_ sender: UIButton) {
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
