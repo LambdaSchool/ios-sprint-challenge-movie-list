2ab29701a33a4d2ab5fce32abcbac572c30b036c
donellabarcelo <donellabarcelo95@gmail.com>
2019-11-08 09:04:25 -0800

    Created controllers and linked them to view controller scenes.

---
+++ Movie List/Views/MovieTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  MovieTableViewCell.swift
+//  Movie List
+//
+//  Created by Donella Barcelo on 11/8/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+protocol MovieTableViewCellDelegate: class {
+    func isSeenButtonTapped(on cell: MovieTableViewCell)
+}
+
+class MovieTableViewCell: UITableViewCell {
+    
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
