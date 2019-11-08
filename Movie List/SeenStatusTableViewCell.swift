cb56b20a8abb86656cbb2aaef8e83164ad63e541
Tobi Tselios <tobi.tselios@gmail.com>
2019-11-08 17:41:16 +0000

    Edits on the views

---
+++ Movie List/SeenStatusTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SeenStatusTableViewCell.swift
+//  Movie List
+//
+//  Created by Tobi Kuyoro on 08/11/2019.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class SeenStatusTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
