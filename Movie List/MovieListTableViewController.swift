e918568ce9a38353405b4bfebd3bd9d798946b14
Tobi Tselios <tobi.tselios@gmail.com>
2019-11-08 19:11:19 +0000

    mostly done

--- Movie List/MovieListTableViewController.swift
+++ Movie List/MovieListTableViewController.swift
@@ -8,23 +8,47 @@
 
 import UIKit
 
-class MovieListTableViewController: UIViewController {
+class MovieListTableViewController: UIViewController, AddMovieDelegate {
     
     @IBOutlet weak var tableView: UITableView!
     
     var movies: [Movie] = []
     
-
+    func movieWasAdded(movie: Movie) {
+        movies.append(movie)
+        tableView.reloadData()
+        dismiss(animated: true, completion: nil)
+    }
     
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
+        if segue.identifier == "AddMovieSegue" {
+            if let addMovieVC = segue.destination as? AddMovieViewController {
+                addMovieVC.delegate = self
+            }
+        }
+    }
 }
+
+extension MovieListTableViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as? SeenStatusTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        let movie = movies[indexPath.row]
+        
+        cell.movieLabel.text = movie.name
+        
+        return cell
+    }
+}
+
