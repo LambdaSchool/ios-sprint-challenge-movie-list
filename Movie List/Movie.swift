6a5203772c848984ea79e3492f1407ba0436450b
sal562 <salmanamer@gmail.com>
2019-11-22 18:07:51 -0800

    first commit project setup

---
+++ Movie List/Movie.swift
@@ -0,0 +1,14 @@
+//
+//  Movie.swift
+//  Movie List
+//
+//  Created by LambdaSchoolVM_Catalina on 11/22/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Movie {
+    var movieName: String
+    var seen: Bool = false
+}
