6194b403de22cd36aca39b4d3e3c54d58fe0076e
sal562 <salmanamer@gmail.com>
2019-11-22 18:33:04 -0800

    storyboardCompleted

---
+++ Movie List/View Controllers/MovieAddViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MovieAddViewController.swift
+//  Movie List
+//
+//  Created by LambdaSchoolVM_Catalina on 11/22/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MovieAddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Movie List/Views/MoviesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MoviesTableViewCell.swift
+//  Movie List
+//
+//  Created by LambdaSchoolVM_Catalina on 11/22/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MoviesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
