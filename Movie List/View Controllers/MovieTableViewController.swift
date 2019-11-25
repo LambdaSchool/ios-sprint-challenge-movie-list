4a08978fdb3fa92e7f1e36c61e22c85d2a3db849
John Holowesko <john@sipsip.io>
2019-11-24 22:19:05 -0500

    First Commit

---
+++ Movie List/View Controllers/MovieTableViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MovieTableViewController.swift
+//  Movie List
+//
+//  Created by John Holowesko on 11/24/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewController: UITableViewController {
+
+    // MARK: IBOutlets
+    @IBOutlet var tableview: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+}
---
+++ Movie List/Views/moviesTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  moviesTableViewCell.swift
+//  Movie List
+//
+//  Created by John Holowesko on 11/24/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class moviesTableViewCell: UITableViewCell {
+
+   // MARK: - IBOutlets
+    
+
+}
