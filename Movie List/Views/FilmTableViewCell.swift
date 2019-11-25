65b0b86fb9768855aa92d8740785a03be31ad706
John Holowesko <john@sipsip.io>
2019-11-24 22:25:12 -0500

    Fixed Version

---
+++ Movie List/Views/FilmTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  FilmTableViewCell.swift
+//  Movie List
+//
+//  Created by John Holowesko on 11/24/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class FilmTableViewCell: UITableViewCell {
+
+    // MARK: - IBOutlets
+    @IBOutlet var movieNameLabel: UILabel!
+    @IBOutlet var seenNotSeenButton: UIButton!
+    
+
+}
