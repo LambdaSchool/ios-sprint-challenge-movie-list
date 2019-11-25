968a51ce42c983f5bfe497c57937164228d565e0
David <davidtwright89@gmail.com>
2019-11-24 21:39:12 -0500

    Added new files to project

---
+++ Movie List/Model/Movie.swift
@@ -0,0 +1,14 @@
+//
+//  Movie.swift
+//  Movie List
+//
+//  Created by David Wright on 11/24/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Movie {
+    let title: String
+    var hasBeenSeen: Bool = false
+}
