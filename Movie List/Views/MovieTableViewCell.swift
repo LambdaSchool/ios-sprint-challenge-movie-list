1f883e37563735dc5bc99b8caf683993e41974d6
David <davidtwright89@gmail.com>
2019-11-24 22:42:42 -0500

    Set up MovieTableViewCell

--- Movie List/Views/MovieTableViewCell.swift
+++ Movie List/Views/MovieTableViewCell.swift
@@ -10,6 +10,32 @@ import UIKit
 
 class MovieTableViewCell: UITableViewCell {
 
+    // MARK: - IBOutlets
+    @IBOutlet var movieTitleLabel: UILabel!
+    @IBOutlet var seenMovieButton: UIButton!
+    
+    // MARK: - IBActions
+    @IBAction func seenMovieButtonTapped(_ sender: UIButton) {
+        if movie != nil {
+            movie!.hasBeenSeen.toggle()
+        }
+        updateViews()
+    }
+    
+    // MARK: - Properties
+    var movie: Movie? {
+        didSet {
+            updateViews()
+        }
+    }
+
+    private func updateViews() {
+        guard let movie = movie else { return }
+        
+        movieTitleLabel.text = movie.title
+        seenMovieButton.setTitle(movie.hasBeenSeen ? "Seen" : "Not Seen", for: .normal)
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
