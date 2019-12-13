411b6bde4edb95972ae376adb070980d87dc9ee8
Jorge Alvarez <jorgelalvarez52@gmail.com>
2019-12-13 08:14:42 -0800

    added model and VC

---
+++ Movie List/Movie.swift
@@ -0,0 +1,14 @@
+//
+//  Movie.swift
+//  Movie List
+//
+//  Created by Jorge Alvarez on 12/13/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Movie {
+    var name: String
+    var seen: Bool = false
+}
---
+++ Movie List/MoviesTableViewController.swift
@@ -0,0 +1,49 @@
+//
+//  MoviesTableViewController.swift
+//  Movie List
+//
+//  Created by Jorge Alvarez on 12/13/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MoviesTableViewController: UIViewController {
+    
+    var moviesArray: [Movie] = [Movie(name: "Saw", seen: true)]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Add later
+        //tableView.delegate = self
+        //tableView.dataSource = self
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension MoviesTableViewController: UITableViewDataSource,  UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return moviesArray.count
+    }
+    
+    // what to put in each CUSTOM cell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+}
