3b8b368df002368ec2fa9e65e10021368418a6ae
Rohan Taylor <rhntaylor@gmail.com>
2019-12-13 20:51:59 -0800

    FINAL

--- MovieList/MovieList/MovieTableViewController.swift
+++ MovieList/MovieList/MovieTableViewController.swift
@@ -10,52 +10,54 @@ import UIKit
 
 class MovieTableViewController: UIViewController {
     
-    @IBOutlet weak var tableView: UITableView!
-    
-    var movies: [Movie] = []
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
+        @IBOutlet weak var tableView: UITableView!
         
-        tableView.dataSource = self
-        tableView.delegate = self
-    }
-    
-    // MARK: - Navigation
-    
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "AddMovieSegue" {
+        var movies: [Movie] = []
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
             
-            if let destinationVC = segue.destination as? MovieViewController {
-                destinationVC.delegate = self
+            tableView.dataSource = self
+            tableView.delegate = self
+        }
+        
+        // MARK: - Navigation
+        
+        // In a storyboard-based application, you will often want to do a little preparation before navigation
+        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            if segue.identifier == "AddMovieSegue" {
+                
+                if let destinationVC = segue.destination as? MovieViewController {
+                    destinationVC.delegate = self
+                }
             }
         }
     }
-}
 
-extension MovieTableViewController: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return movies.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as? MovieCellInfo else {
-            
-            fatalError()
-            
+    extension MovieTableViewController: UITableViewDelegate, UITableViewDataSource {
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return movies.count
         }
-        let movie = movies[indexPath.row]
-        cell.movie = movie
         
-        return cell
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as? MovieTableViewCell else {
+                
+                fatalError()
+                
+            }
+            let movie = movies[indexPath.row]
+            cell.movie = movie
+            
+            return cell
+        }
     }
-}
-extension MovieTableViewController : MovieDelegate {
-    
-    func newMovie(movie: Movie) {
-        movies.append(movie)
-        tableView.reloadData()
+    extension MovieTableViewController : MovieDelegate {
+        
+        func newMovie(movie: Movie) {
+            movies.append(movie)
+            tableView.reloadData()
+        }
     }
-}
+
+
 
