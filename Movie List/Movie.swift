917da6fab22be6c3eb327517c082f96b82e6dba8
Rebecca <mythreldrows@aol.com>
2020-01-24 11:58:33 -0500

    Storyboard created and constrained

---
+++ Movie List/Movie.swift
@@ -0,0 +1,14 @@
+//
+//  Movie.swift
+//  Movie List
+//
+//  Created by Lambda_School_Loaner_254 on 1/24/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Movie {
+    let movieTitle: String
+}
+	
