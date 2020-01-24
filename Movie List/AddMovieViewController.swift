874e518bf3e6d7642526fd429ec18c4793bb753c
Chris Gonzales <57194564+supergonzo812@users.noreply.github.com>
2020-01-24 08:37:51 -0800

    Created outlets for Add button on MovieListTableVC, textfield on AddMovieVC, and save button on AddMovieVCV

---
+++ Movie List/AddMovieViewController.swift
@@ -0,0 +1,38 @@
+//
+//  AddMovieViewController.swift
+//  Movie List
+//
+//  Created by Chris Gonzales on 1/24/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class AddMovieViewController: UIViewController {
+
+    
+    @IBAction func AddMovieTextField(_ sender: UITextField) {
+    }
+    
+    @IBAction func SaveButton(_ sender: UIButton) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Movie List/MovieListTableViewController.swift
@@ -0,0 +1,48 @@
+//
+//  MovieListTableViewController.swift
+//  Movie List
+//
+//  Created by Chris Gonzales on 1/24/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+extension MovieListTableViewCell: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+}
+
+class MovieListTableViewController: UIViewController{
+
+    var movies = [Movie]()
+    
+    @IBAction func AddMovieButton(_ sender: UIBarButtonItem) {
+    }
+    @IBAction func SeenToggleButton(_ sender: UIButton) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
