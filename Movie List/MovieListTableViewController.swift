8abc90ee6e413fd01ce68b84bdf68ed935ddfc6f
Chris Gonzales <57194564+supergonzo812@users.noreply.github.com>
2020-01-24 11:08:47 -0800

    first stretch goal complete. swipe to delete

--- Movie List/MovieListTableViewController.swift
+++ Movie List/MovieListTableViewController.swift
@@ -30,29 +30,31 @@ extension MovieListTableViewController: UITableViewDataSource{
         return cell
     }
     
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if (editingStyle == .delete) {
+            movies.remove(at: indexPath.row)
+            MovieTableView.reloadData()
+        }
+    }
     
 }
 
 class MovieListTableViewController: UIViewController{
 
     var movies = [Movie]()
-    
 
     @IBOutlet weak var MovieTableView: UITableView!
     @IBAction func AddMovieButton(_ sender: UIBarButtonItem) {
     }
  
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-
-  
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "AddNewMovieSegue"{
             if let addMovieVC = segue.destination as? AddMovieViewController{
