8f5123fad0beb4c509d9ce31088be8d2e8720c4a
Breena Greek <bgreek2019@gmail.com>
2020-02-07 21:31:12 -0500

    Setup

---
+++ Movie List/View Controllers/MovieListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieListTableViewCell.swift
+//  Movie List
+//
+//  Created by Breena Greek on 2/7/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MovieListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
