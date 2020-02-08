d06e7295da17680fc0e1378031f403132591ccfd
Claudia Maciel <clc_80@yahoo.com>
2020-02-07 18:12:52 -0800

    Movie Object added

---
+++ Movie List/Model/Movie.swift
@@ -0,0 +1,14 @@
+//
+//  Movie.swift
+//  Movie List
+//
+//  Created by Claudia Contreras on 2/7/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Movie {
+    var name: String
+    var hasSeen: Bool = false
+}
