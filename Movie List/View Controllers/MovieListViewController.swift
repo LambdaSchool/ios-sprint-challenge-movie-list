4fcfecb60c15dc56a4c9745ac9a4be485455069d
Chad Parker <chad@chadparker.com>
2020-02-07 20:13:48 -0800

    Enable deleting rows

--- Movie List/View Controllers/MovieListViewController.swift
+++ Movie List/View Controllers/MovieListViewController.swift
@@ -45,4 +45,11 @@ extension MovieListViewController: UITableViewDataSource {
         cell.movie = movieList[indexPath.row]
         return cell
     }
+
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            movieList.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
 }
