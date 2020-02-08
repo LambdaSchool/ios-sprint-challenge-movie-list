cb58fd642051c975a1f24d2778ad1dcfaa0360ba
Robert Vance <Fearmydotzs@gmail.com>
2020-02-07 22:09:54 -0700

    Movie List Challenge

---
+++ Movie List/MovieStruct.swift
@@ -0,0 +1,14 @@
+//
+//  MovieStruct.swift
+//  Movie List
+//
+//  Created by Robert on 2/7/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+
+struct Movie {
+    var movieName: String
+}
