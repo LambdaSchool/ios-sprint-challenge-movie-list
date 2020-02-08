38038c315eea272d8f81658c7ea7dc4aeb0982d5
Claudia Maciel <clc_80@yahoo.com>
2020-02-07 18:47:24 -0800

    movieViewController shows data

--- Movie List/ViewControllers/MovieViewController.swift
+++ Movie List/ViewControllers/MovieViewController.swift
@@ -13,9 +13,14 @@ class MovieViewController: UIViewController {
     //IBOutlets
     @IBOutlet var tableView: UITableView!
     
+    //Properties
+    var movies: [Movie] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
 
         // Do any additional setup after loading the view.
     }
@@ -23,4 +28,21 @@ class MovieViewController: UIViewController {
     
     @IBAction func addMovieButtonTapped(_ sender: Any) {
     }
+}
+
+//Extensions
+
+extension MovieViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as? MovieTableViewCell else {return UITableViewCell()}
+        let movie = movies[indexPath.row]
+        cell.movie = movie
+        return cell
+    }
+    
+    
 }
