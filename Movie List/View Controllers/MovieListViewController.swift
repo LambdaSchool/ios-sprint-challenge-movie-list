935265ba8f92aa4ea04a45abbf7e7119c0771837
Chad Parker <chad@chadparker.com>
2020-02-07 18:59:07 -0800

    Add view controllers & link to storyboard views

---
+++ Movie List/View Controllers/MovieListViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MovieListViewController.swift
+//  Movie List
+//
+//  Created by Chad Parker on 2/7/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MovieListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+}
---
+++ Movie List/View Controllers/MovieTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  MovieTableViewCell.swift
+//  Movie List
+//
+//  Created by Chad Parker on 2/7/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
