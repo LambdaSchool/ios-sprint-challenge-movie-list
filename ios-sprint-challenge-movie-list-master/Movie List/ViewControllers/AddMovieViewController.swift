a258edf582a78e33fbd26c3a976cafda321c1cd1
Jordan Skenandore <jordan.skenandore@gmail.com>
2020-02-07 20:58:40 -0700

    Add constraints to main.storyboard within the addmovieVC

--- ios-sprint-challenge-movie-list-master/Movie List/ViewControllers/AddMovieViewController.swift
+++ ios-sprint-challenge-movie-list-master/Movie List/ViewControllers/AddMovieViewController.swift
@@ -29,15 +29,14 @@ class AddMovieViewController: UIViewController {
         
         delegate?.movieWasAdded(movieInstance)
     }
-    
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension AddMovieViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if let text = textField.text,
+            !text.isEmpty {
+            textField.resignFirstResponder()
+        }
+        return false
     }
-    */
-
 }
