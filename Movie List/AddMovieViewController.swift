23ee1e441a8f17d3366ad9514a72b28f673cd20c
Shawn Gee <shawngee@gmail.com>
2020-02-21 11:24:07 -0500

    Storyboard initial setup

---
+++ Movie List/AddMovieViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddMovieViewController.swift
+//  Movie List
+//
+//  Created by Shawn Gee on 2/21/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class AddMovieViewController: UIViewController {
+    
+    @IBOutlet weak var movieNameTextField: UITextField!
+    
+    @IBAction func addButtonTapped(_ sender: UIButton) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
