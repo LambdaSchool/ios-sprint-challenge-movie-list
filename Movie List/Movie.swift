d69a44d40966cb1a5f3e8342860711d0d1192a30
Mark Gerrior <github@gerrior.com>
2020-02-21 08:44:46 -0800

    Added Movie struct.

---
+++ Movie List/Movie.swift
@@ -0,0 +1,19 @@
+//
+//  Movie.swift
+//  Movie List
+//
+//  Created by Mark Gerrior on 2/21/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Movie {
+    var title: String
+    var seen: Bool
+    
+    init() {
+        title = ""
+        seen = false
+    }
+}
