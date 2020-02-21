b915ccb6ad9417892981a8830c0eb9fdc0c6f3f6
Hector L <hectoremil21@hotmail.com>
2020-02-21 12:20:37 -0500

    Add all files. Organize under MVC convention files.

---
+++ Movie List/Model/Movie.swift
@@ -0,0 +1,9 @@
+//
+//  Movie.swift
+//  Movie List
+//
+//  Created by Karen Rodriguez on 2/21/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
---
+++ Movie List/View Controllers/MoviesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MoviesViewController.swift
+//  Movie List
+//
+//  Created by Karen Rodriguez on 2/21/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MoviesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
