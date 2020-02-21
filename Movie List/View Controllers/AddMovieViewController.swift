7869cf3df8f34a5c439b8ed332c6420c72975798
Diego Covarrubias <dcov@pm.me>
2020-02-21 08:56:26 -0800

    Add AddMovieViewController, AddMovie scene and ShowAddMovieSegue segue

---
+++ Movie List/View Controllers/AddMovieViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AddMovieViewController.swift
+//  Movie List
+//
+//  Created by dc on 2/21/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class AddMovieViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
