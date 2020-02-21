b7bfbf696f87d3c25fb8dd87cf81bcfb13ca0373
Diego Covarrubias <dcov@pm.me>
2020-02-21 08:54:19 -0800

    Add TableView outlet to view controller, Add MoveTableViewCell class and MovieCell identifier

--- Movie List/View Controllers/MoveListViewController.swift
+++ Movie List/View Controllers/MoveListViewController.swift
@@ -10,9 +10,9 @@ import UIKit
 
 class MoveListViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 }
