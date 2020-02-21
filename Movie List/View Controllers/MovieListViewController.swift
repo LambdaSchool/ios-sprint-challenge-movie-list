afd5064a53ad6ddb1a1cda8f5c30725aacd16cad
Diego Covarrubias <dcov@pm.me>
2020-02-21 09:18:47 -0800

    Implement UITableViewDelegate and UITableViewDataSource for MovieListViewController and set it as such

--- Movie List/View Controllers/MovieListViewController.swift
+++ Movie List/View Controllers/MovieListViewController.swift
@@ -13,6 +13,12 @@ class MovieListViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
     var movies = Array<Movie>()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
 }
 
 extension MovieListViewController: AddMovieDelegate {
@@ -20,5 +26,22 @@ extension MovieListViewController: AddMovieDelegate {
     func didAddMovie(_ movie: Movie) {
         movies.append(movie)
         tableView.reloadData()
+    }
+}
+
+extension MovieListViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath)
+        
+        if let movieCell = cell as? MovieTableViewCell {
+            movieCell.movie = movies[indexPath.row]
+        }
+        
+        return cell
     }
 }
