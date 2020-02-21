afd431882cf59a9bea7da4d9edae6452e514d29e
Wyatt Harrell <wyattaharrell@gmail.com>
2020-02-21 11:20:19 -0500

    initial setup

---
+++ Movie List/MovieTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieTableViewCell.swift
+//  Movie List
+//
+//  Created by Wyatt Harrell on 2/21/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var movieTitleLabel: UILabel!
+    
+    @IBOutlet weak var hasSeenButton: UIButton!
+    
+
+    
+    
+    
+    
+    
+    
+}
---
+++ Movie List/MoviesListViewController.swift
@@ -0,0 +1,57 @@
+//
+//  MoviesListViewController.swift
+//  Movie List
+//
+//  Created by Wyatt Harrell on 2/21/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MoviesListViewController: UIViewController, UITableViewDataSource {
+
+    var movies: [Movie] = [Movie(name: "No Time To Die", hasSeen: false)]
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath)
+        
+        
+        
+        
+        
+        
+        return cell
+    }
+    
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+       
+    
+        
+        
+        
+    }
+    
+
+}
