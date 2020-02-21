581bdc084d4f4e72f6174c8fece2a1617638dc88
Nichole Davidson <nicholedavid@gmail.com>
2020-02-21 09:46:10 -0700

    Initial setup

--- Movie List/Movie.swift
+++ Movie List/Movie.swift
@@ -2,17 +2,13 @@
 //  Movie.swift
 //  Movie List
 //
-//  Created by Nichole Davidson on 1/24/20.
+//  Created by Nichole Davidson on 2/21/20.
 //  Copyright © 2020 Lambda School. All rights reserved.
 //
 
 import Foundation
 
-class Movie {
+struct Movie {
     var name: String
     var seen: Bool = false
-    
-    init(name: String) {
-    self.name = name
- }
 }
--- Movie List/MovieListViewController.swift
+++ Movie List/MovieListViewController.swift
@@ -2,57 +2,32 @@
 //  MovieListViewController.swift
 //  Movie List
 //
-//  Created by Nichole Davidson on 1/24/20.
+//  Created by Nichole Davidson on 2/21/20.
 //  Copyright © 2020 Lambda School. All rights reserved.
 //
 
 import UIKit
 
-class MovieListViewController: UIViewController, MovieAddedDelegate, UITableViewDataSource, SeenButtonDelegate {
-    func seenButtonTapped(cell: UITableViewCell) {
-        guard let indexPath = tableView.indexPath(for: cell) else { return }
-         var movie = moviesOnList[indexPath.row]
-        movie.seen.toggle()
-        tableView.reloadData()
-    }
+class MovieListViewController: UIViewController {
     
-   
-    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var movieListTableView: UITableView!
     
-    var moviesOnList: [Movie] = []
 
-    
     override func viewDidLoad() {
-           super.viewDidLoad()
-        
-       }
-    
-    func movieWasAdded(_ movie: Movie) {
-        moviesOnList.append(movie)
-//            dismiss(animated: true, completion: nil)
-            tableView.reloadData()
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return moviesOnList.count
-    }
-       
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-           guard let cell = tableView.dequeueReusableCell(withIdentifier: "MovieOnListCell", for: indexPath) as? MovieNotSeenTableViewCell else { return UITableViewCell() }
-           
-           let movie = moviesOnList[indexPath.row]
-           cell.movie = movie
-            cell.delegate = self
-           
-           return cell
-       }
-    
+
+    /*
     // MARK: - Navigation
 
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "ShowAddMovie" {
-            guard let addMovieVC = segue.destination as? AddMovieViewController else { return }
-                addMovieVC.delegate = self
-        }
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
     }
-  }
+    */
+
+}
