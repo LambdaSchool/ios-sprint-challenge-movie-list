bb05b8bebe4778dd23d4f1607c6b22a977b341b9
Chris Dobek <chrisdobek@MacBook-Pro.local>
2020-03-20 11:44:39 -0400

    Auto Layout Complete

---
+++ Movie List/Movie.swift
@@ -0,0 +1,9 @@
+//
+//  Movie.swift
+//  Movie List
+//
+//  Created by Chris Dobek on 3/20/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
---
+++ Movie List/NewMovieViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NewMovieViewController.swift
+//  Movie List
+//
+//  Created by Chris Dobek on 3/20/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class NewMovieViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
