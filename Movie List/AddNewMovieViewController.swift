bd7977edb79524606bbd36fd10fcad69ec13065b
Harmony Radley <harmonyradley99@gmail.com>
2020-03-20 12:07:20 -0400

    Added Two view controllers

---
+++ Movie List/AddNewMovieViewController.swift
@@ -0,0 +1,53 @@
+//
+//  AddNewMovieViewController.swift
+//  Movie List
+//
+//  Created by Harmony Radley on 3/20/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+protocol AddMovieDelegate {
+    func MovieWasCreated(_ mov: Movie)
+}
+
+class AddNewMovieViewController: UIViewController {
+    
+    @IBOutlet weak var movieNameTextField: UITextField!
+    
+    
+    var delegate: AddMovieDelegate?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+    
+    @IBAction func addMovieTapped(_ sender: UIBarButtonItem) {
+        guard let movieName = movieNameTextField.text,
+            !movieName.isEmpty else { return }
+        
+        let movie = Movie(name: movieName, seen: true)
+        
+        delegate?.MovieWasCreated(movie)
+        navigationController?.popViewController(animated: true)
+    }
+}
+
+extension AddNewMovieViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if let text = textField.text,
+            !text.isEmpty {
+            switch textField {
+            case movieNameTextField:
+                movieNameTextField.becomeFirstResponder()
+            default:
+                textField.resignFirstResponder()
+            }
+        }
+        
+        return false
+    }
+}
+
