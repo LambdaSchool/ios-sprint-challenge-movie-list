80a5893a121b904dafcbcaeb630c2017a344ca37
Shawn-James <shawn.james@me.com>
2020-03-20 11:15:40 -0400

    organize project and add files

---
+++ Movie List/Controller/MovieTableTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieTableTableViewCell.swift
+//  Movie List
+//
+//  Created by Shawn James on 3/20/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
