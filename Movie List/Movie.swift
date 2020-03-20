27eab02b362cd89d1ec434b6dbf3c3c0d8cf2af0
Cody Morley <codymorleyinc@gmail.com>
2020-03-20 08:58:55 -0700

    basic framework finished. Not confident about anything after this.

---
+++ Movie List/Movie.swift
@@ -0,0 +1,14 @@
+//
+//  Movie.swift
+//  Movie List
+//
+//  Created by Cody Morley on 3/20/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Movie {
+    let name: String
+    let haveSeen: Bool = false
+}
---
+++ Movie List/MovieViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieViewCell.swift
+//  Movie List
+//
+//  Created by Cody Morley on 3/20/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MovieViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
