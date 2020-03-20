fc8fbc9c894eeedf70c85f33e39c20c999d5fd75
Cameron Collins <cameroncollins19@yahoo.com>
2020-03-20 10:30:39 -0500

    Added Controllers and set up storyboard

---
+++ Movie List/AddMovieViewController.swift
@@ -0,0 +1,32 @@
+//
+//  AddMovieViewController.swift
+//  Movie List
+//
+//  Created by Cameron Collins on 3/20/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class AddMovieViewController: UIViewController {
+
+    //Variables
+    var previousController: MovieListViewController?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+    
+
+}
---
+++ Movie List/MovieListViewController.swift
@@ -0,0 +1,37 @@
+//
+//  MovieListViewController.swift
+//  Movie List
+//
+//  Created by Cameron Collins on 3/20/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MovieListViewController: UIViewController, UITableViewDataSource {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    //Table View
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        
+        
+    }
+    
+
+}
