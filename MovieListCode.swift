634ee78a39346561dd0fb6b1c7d968386a9de0e0
violet-love <violetlove9419@icloud.com>
2020-03-20 10:34:21 -0700

    Started some code

---
+++ MovieListCode.swift
@@ -0,0 +1,15 @@
+//
+//  MovieListCode.swift
+//  Movie List
+//
+//  Created by Violet Lavender Love on 3/20/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+/*struct NotSureWhatToCallYou {
+    let something = something
+    var spmething = something
+    
+}
+*
