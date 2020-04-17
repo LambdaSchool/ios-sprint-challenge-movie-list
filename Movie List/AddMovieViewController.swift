4e76bf5138d613bedd3aaeca98ff22798912a847
Ronald Huston Jr <rghuston9@gmail.com>
2020-04-17 12:29:46 -0500

    MovieTableViewCell

---
+++ Movie List/AddMovieViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AddMovieViewController.swift
+//  Movie List
+//
+//  Created by ronald huston jr on 4/17/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+protocol NewMovieDelegate {
+    func movieWasCreated(movie: Movie)
+}
+class AddMovieViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
