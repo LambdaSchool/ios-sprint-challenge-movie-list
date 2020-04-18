deb5c48e1778f804f90b71dffd4db03b87be454a
Michael Self <mikeyself@gmail.com>
2020-04-17 20:34:35 -0700

    all done i guess

---
+++ Movie List/Movies.swift
@@ -0,0 +1,14 @@
+//
+//  Movies.swift
+//  Movie List
+//
+//  Created by Michael Self on 4/17/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Movie {
+    var movieNames: String
+    var seenMovie: Bool = false 
+}
