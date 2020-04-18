e3afc2dd312af145b9e31402b4346d0d997cede6
Your Name Here <youremailhere>
2020-04-17 22:48:25 -0400

    Added Custom TableViewContollerCells button lay outs and constrants

---
+++ Movie List/ViewControllers/AddMovieViewController.swift
@@ -0,0 +1,36 @@
+//
+//  AddMovieViewController.swift
+//  Movie List
+//
+//  Created by Sammy Alvarado on 4/17/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class AddMovieViewController: UIViewController {
+
+    @IBOutlet weak var movieTitleTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+    
+    @IBAction func addMovieTapped(_ sender: Any) {
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
