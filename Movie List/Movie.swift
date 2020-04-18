3f12b41d4c76402df304594b5e9960f287468807
Bronson <bronsonmullens@icloud.com>
2020-04-17 18:40:37 -0700

    Initial Commit

---
+++ Movie List/Movie.swift
@@ -0,0 +1,14 @@
+//
+//  Movie.swift
+//  Movie List
+//
+//  Created by Bronson Mullens on 4/17/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Movie {
+    var movieName: String
+    var hasSeen: Bool = false
+}
