9d816c404fff83335f931661342e85d25f43c4f1
Hannah <hannahlbain89@gmail.com>
2020-04-18 13:28:46 -0500

    Adding some code

---
+++ Movie List/Movie.swift
@@ -0,0 +1,29 @@
+//
+//  Movie.swift
+//  Movie List
+//
+//  Created by Hannah Bain on 4/18/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class Movie {
+    var movieName: String
+    var seen: Bool
+    
+    init(movieName: String, seen: Bool) {
+        self.movieName = movieName
+        self.seen = seen
+    }
+    func movieSeen() {
+            if seen == true {
+
+            }
+        }
+    }
+
+
+    
+
+
