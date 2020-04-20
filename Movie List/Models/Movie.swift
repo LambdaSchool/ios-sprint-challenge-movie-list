8bd9163283dfc32f2392c2989f7dfe1adee0abb2
Ian French <igf2011@hotmail.com>
2020-04-19 21:42:11 -0400

    First Commit

---
+++ Movie List/Models/Movie.swift
@@ -0,0 +1,15 @@
+//
+//  Movie.swift
+//  Movie List
+//
+//  Created by Ian French on 4/19/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+
+struct Movie {
+    var hasSeen: Bool = false
+    var movieName: String
+}
