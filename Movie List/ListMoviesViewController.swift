a4aa8a9d3ba3e4b045476e4c20a95e73f50e1265
Mike Nichols <mike@nixco.net>
2020-05-15 10:37:42 -0500

    Screen setup complete-ish. @mrflowers22

---
+++ Movie List/ListMoviesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ListMoviesViewController.swift
+//  Movie List
+//
+//  Created by Mike Nichols on 5/15/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ListMoviesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Movie List/movieslist.swift
@@ -0,0 +1,14 @@
+//
+//  movieslist.swift
+//  Movie List
+//
+//  Created by Mike Nichols on 5/15/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Movie {
+    let title: String
+    var haveSeen: Bool = false
+}
