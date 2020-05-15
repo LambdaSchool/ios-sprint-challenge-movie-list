fa1bc3058fabe881541ffa8e60fd192e24e05065
Bryan Cress <8675309+cressbj@users.noreply.github.com>
2020-05-15 12:46:54 -0400

    created scense on main storyboard and corresponding files

---
+++ Models/Movie.swift
@@ -0,0 +1,15 @@
+//
+//  Movie.swift
+//  Movie List
+//
+//  Created by Bryan Cress on 5/15/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+
+struct Movie {
+    let name: String
+    let hasBeenSeen: Bool = false
+}
