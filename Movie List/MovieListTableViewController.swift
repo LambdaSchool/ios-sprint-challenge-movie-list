93d064397603ec510cee6b1491c035841754a770
dmills14 <drew-miller@lambdastudents.com>
2020-05-15 09:09:04 -0700

    basic layout

--- Movie List/MovieListTableViewController.swift
+++ Movie List/MovieListTableViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class MovieListTableViewController: UITableViewController {
 
+    var movies: [Movie] = [Movie]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -24,13 +26,13 @@ class MovieListTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return movies.count
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    // override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
+        
+
 
     /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
