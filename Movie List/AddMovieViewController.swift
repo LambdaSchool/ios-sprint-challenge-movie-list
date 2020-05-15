2c9fda2055d810735cca47abb67b36f7f971ede5
Ian Becker <ian-becker@lambdastudents.com>
2020-05-15 09:24:15 -0700

    Created files and outlets to the corresponding scenes.

---
+++ Movie List/AddMovieViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AddMovieViewController.swift
+//  Movie List
+//
+//  Created by Ian Becker on 5/15/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class AddMovieViewController: UIViewController {
+    
+    @IBOutlet var addMovieViewController: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
