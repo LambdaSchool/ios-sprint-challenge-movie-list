5db997f55ba6f8a1c169f2af61c164796dbf4374
Sam Gutierrez <Samguti02@gmail.com>
2020-05-30 11:58:17 -0600

    Connected outlets and action items to the corresponding ViewControllers

---
+++ Movie List/View Controllers/AddingMovieViewController.swift
@@ -0,0 +1,36 @@
+//
+//  AddingMovieViewController.swift
+//  Movie List
+//
+//  Created by Sam Gutierrez on 5/30/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class AddingMovieViewController: UIViewController {
+
+    
+    @IBOutlet weak var EnterMoviesTitleTextLabel: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func AddMovieButtonTapped(_ sender: Any) {
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Movie List/View Controllers/MoviesTableViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MoviesTableViewController.swift
+//  Movie List
+//
+//  Created by Sam Gutierrez on 5/30/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MoviesTableViewController: UIViewController {
+    
+    @IBOutlet weak var MovieTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
