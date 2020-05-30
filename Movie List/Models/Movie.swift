a25c20c2db701a9c54c8499d54fe9daa2f39db48
Sam Gutierrez <Samguti02@gmail.com>
2020-05-30 10:16:39 -0600

    Added Movie model to my project

---
+++ Movie List/Models/Movie.swift
@@ -0,0 +1,14 @@
+//
+//  Movie.swift
+//  Movie List
+//
+//  Created by Sam Gutierrez on 5/30/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Movie {
+    let name: String
+    let watched: Bool = false 
+}
