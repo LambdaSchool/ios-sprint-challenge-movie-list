e130b9702ad0271da3f33ebb2423b65bb74613ef
Eugene White <eugenewhite1994@gmail.com>
2020-05-29 23:52:58 -0400

    This is what i finished

---
+++ Movie List/Movie.swift
@@ -0,0 +1,15 @@
+//
+//  Movie.swift
+//  Movie List
+//
+//  Created by Eugene White on 5/29/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+
+struct MovieList {
+    var movieName: String
+    var hasBeenSeen: Bool
+}
