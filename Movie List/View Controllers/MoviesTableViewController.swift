8dda8b9b6b147594c10d0e809865f73caadae753
Sam Gutierrez <Samguti02@gmail.com>
2020-05-30 13:03:25 -0600

    Almost Done

--- Movie List/View Controllers/MoviesTableViewController.swift
+++ Movie List/View Controllers/MoviesTableViewController.swift
@@ -22,12 +22,6 @@ class MoviesTableViewController: UIViewController {
         MovieTableView.dataSource = self
         
     }
-    
-    @IBAction func SeenNotSeenButtonTapped(_ sender: Any) {
-        
-    }
-    
-    
 }
 
 
