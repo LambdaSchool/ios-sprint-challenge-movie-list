4a88030078e524d17395c7b8e835400d45ab01d9
Ben James <bejjmail@gmail.com>
2020-05-31 21:30:28 -0700

    coding mainly done, now getting everything to work

---
+++ Movie List/MovieControllerProtocol.swift
@@ -0,0 +1,9 @@
+//
+//  MovieControllerProtocol.swift
+//  Movie List
+//
+//  Created by Dojo on 5/31/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
---
+++ Movie List/MoviesTabBarController.swift
@@ -0,0 +1,9 @@
+//
+//  MoviesTabBarController.swift
+//  Movie List
+//
+//  Created by Dojo on 5/31/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
