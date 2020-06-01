41801f1ef23dff2b72a891682edf2d36582a8d08
Ben James <bejjmail@gmail.com>
2020-06-01 14:20:31 -0700

    added missing files

--- Movie List/MovieController.swift
+++ Movie List/MovieController.swift
@@ -6,4 +6,18 @@
 //  Copyright © 2020 Lambda School. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class MovieController {
+    var movies: [Movie] = []
+    
+    func create(name: String) {
+        movies.append(Movie.init(name: name))
+    }
+    
+    func deleteMovie(movie: Movie) {
+        guard let index = movies.firstIndex(of: movie) else { return }
+        movies.remove(at: index)
+        print(Movie.self)
+    }
+}
