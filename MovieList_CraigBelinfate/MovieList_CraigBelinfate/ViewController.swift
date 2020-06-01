fd081bfcabf6b4cacfe485f0c29e696a5695f881
Craig Belinfante <craigbasketball@aol.com>
2020-05-31 19:13:37 -0700

    beginning work on Sprint 2

---
+++ MovieList_CraigBelinfate/MovieList_CraigBelinfate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieList_CraigBelinfate
+//
+//  Created by Craig Belinfante on 5/31/20.
+//  Copyright © 2020 Craig Belinfante. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
